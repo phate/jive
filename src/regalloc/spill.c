b75eafb3597ffd57fa3d28481fc3d208f89361c7
Helge Bahmann <hcb@chaoticmind.net>
2010-04-13 20:19:52 +0200

    Track stack slots in spill/restore instructions
    Reference the used stack slots in instructions
    storing or loading values to/from locations on
    the stack during spill.

--- src/regalloc/spill.c
+++ src/regalloc/spill.c
@@ -6,12 +6,19 @@ jive_regalloc_spill(jive_value * value, const jive_machine * machine, jive_stack
 {
 	jive_stackslot * slot = jive_stackframe_allocate_slot(frame, jive_value_get_cpureg_class(value));
 	
-	return machine->spill(machine, value, slot, frame);
+	jive_node * node = machine->spill(machine, value, slot, frame);
+	
+	jive_instruction_use_stackslot(node, slot);
+	return node;
 }
 
 jive_value *
 jive_regalloc_restore(jive_node * spill, const jive_machine * machine, jive_stackframe * frame)
 {
-	/* FIXME: get hold of the stack slot */
-	return machine->restore(machine, spill->graph, 0, frame);
+	jive_stackslot * slot = jive_instruction_get_stackslot(spill);
+	
+	jive_value * value = machine->restore(machine, spill->graph, 0, frame);
+	jive_instruction_use_stackslot(value->node, slot);
+	
+	return value;
 }
