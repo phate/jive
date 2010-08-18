0d3e6a2754a1c304ab491734c1f2872a96ee91d5
Helge Bahmann <hcb@chaoticmind.net>
2010-08-18 21:04:09 +0200

    Add definitions for control edges

---
+++ include/jive/vsdg/control.h
@@ -0,0 +1,16 @@
+#ifndef JIVE_VSDG_CONTROL_H
+#define JIVE_VSDG_CONTROL_H
+
+#include <jive/vsdg/node.h>
+
+extern const jive_node_class JIVE_ABSTRACT_GAMMA_SLAVE_NODE;
+extern const jive_node_class JIVE_ABSTRACT_GAMMA_MASTER_NODE;
+extern const jive_node_class JIVE_GAMMA_SLAVE_NODE;
+extern const jive_node_class JIVE_GAMMA_MASTER_NODE;
+
+typedef jive_node jive_abstract_gamma_slave_node;
+typedef jive_node jive_abstract_gamma_master_node;
+typedef jive_node jive_gamma_slave_node;
+typedef jive_node jive_gamma_master_node;
+
+#endif
