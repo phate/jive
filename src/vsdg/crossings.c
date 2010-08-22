6b6229ed4b14876db80b05f00827dc6473236f92
Helge Bahmann <hcb@chaoticmind.net>
2010-08-22 19:45:55 +0200

    Don't open-code access to hash buckets
    Use proper accessor functions instead.

--- src/vsdg/crossings.c
+++ src/vsdg/crossings.c
@@ -5,12 +5,9 @@ void
 jive_node_resource_interaction_destroy(jive_node_resource_interaction * self)
 {
 	jive_context * context = self->node->graph->context;
-	size_t hash_by_resource = ((size_t) self->resource) %(self->node->resource_interaction.nbuckets);
 	
 	JIVE_LIST_REMOVE(self->resource->node_interaction, self, same_resource_list);
-	JIVE_LIST_REMOVE(self->node->resource_interaction.buckets[hash_by_resource], self, same_node_list);
-	
-	self->node->resource_interaction.nitems --;
+	jive_resource_interaction_remove(&self->node->resource_interaction, self);
 	
 	jive_context_free(context, self);
 }
