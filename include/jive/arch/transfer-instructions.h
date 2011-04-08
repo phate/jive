da1cfb15d872eee769b2ec3fb74dbffed552020c
Helge Bahmann <hcb@chaoticmind.net>
2011-04-08 08:16:16 +0200

    arch: change interface to xfer instruction factory
    Only one function is needed to deal with the generic concept of
    "split" nodes.

--- include/jive/arch/transfer-instructions.h
+++ include/jive/arch/transfer-instructions.h
@@ -8,19 +8,19 @@ struct jive_output;
 struct jive_input;
 struct jive_node;
 
+typedef struct jive_xfer_block jive_xfer_block;
+
 typedef struct jive_transfer_instructions_factory jive_transfer_instructions_factory;
 
+struct jive_xfer_block {
+	jive_input * input;
+	jive_node * node;
+	jive_output * output;
+};
+
 struct jive_transfer_instructions_factory {
-	size_t (*create_copy)(struct jive_region * region, struct jive_output * origin,
-		struct jive_input ** xfer_in, struct jive_node * xfer_nodes[], struct jive_output ** xfer_out);
-	
-	size_t (*create_spill)(struct jive_region * region, struct jive_output * origin,
-		struct jive_input **spill_in, struct jive_node * spill_nodes[], struct jive_node ** store_node);
-		
-	size_t (*create_restore)(struct jive_region * region, struct jive_output * stackslot,
-		struct jive_node ** load_node, struct jive_node * restore_nodes[], struct jive_output ** restore_out);
-	
-	size_t max_nodes;
+	jive_xfer_block (*create_xfer)(struct jive_region * region, struct jive_output * origin,
+		const struct jive_resource_class * in_class, const struct jive_resource_class * out_class);
 };
 
 #endif
