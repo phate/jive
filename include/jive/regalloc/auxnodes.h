19fb47cae9d91fea89314a1c1581345650ccbd47
Helge Bahmann <hcb@chaoticmind.net>
2011-04-08 08:17:16 +0200

    regalloc: aux split node
    Represent required lifetime splits during register allocation
    as generic auxiliary split node that unifies spill, restore
    and register transfer.

--- include/jive/regalloc/auxnodes.h
+++ include/jive/regalloc/auxnodes.h
@@ -8,8 +8,44 @@ outside the register allocator). */
 
 #include <jive/vsdg/node.h>
 
-struct jive_regcls;
+struct jive_resource_class;
+struct jive_shaped_graph;
 struct jive_transfer_instructions_factory;
+
+typedef struct jive_aux_split_node_attrs jive_aux_split_node_attrs;
+
+struct jive_aux_split_node_attrs {
+	const struct jive_resource_class * in_class;
+	const struct jive_resource_class * out_class;
+};
+
+struct jive_aux_split_node {
+	jive_node base;
+	jive_aux_split_node_attrs attrs;
+};
+
+typedef struct jive_aux_split_node jive_aux_split_node;
+extern const jive_node_class JIVE_AUX_SPLIT_NODE;
+
+jive_node *
+jive_aux_split_node_create(struct jive_region * region,
+	const jive_type * in_type,
+	struct jive_output * in_origin,
+	const struct jive_resource_class * in_class,
+	const jive_type * out_type,
+	const struct jive_resource_class * out_class);
+
+static inline jive_aux_split_node *
+jive_aux_split_node_cast(jive_node * self)
+{
+	if (self->class_ == &JIVE_AUX_SPLIT_NODE) return (jive_aux_split_node *) self;
+	else return 0;
+}
+
+void
+jive_regalloc_auxnodes_replace(struct jive_shaped_graph * shaped_graph, const struct jive_transfer_instructions_factory * gen);
+
+#if 0
 
 typedef struct jive_aux_valuecopy_node jive_aux_valuecopy_node;
 typedef struct jive_aux_spill_node jive_aux_spill_node;
@@ -51,7 +87,6 @@ jive_aux_spill_node_create(struct jive_region * region, const struct jive_regcls
 jive_node *
 jive_aux_restore_node_create(struct jive_region * region, const struct jive_regcls * regcls, struct jive_output * stackslot);
 
-void
-jive_regalloc_auxnodes_replace(struct jive_graph * graph, const struct jive_transfer_instructions_factory * gen);
+#endif
 
 #endif
--- src/regalloc/auxnodes.c
+++ src/regalloc/auxnodes.c
@@ -1,71 +1,127 @@
 #include <jive/regalloc/auxnodes.h>
-#include <jive/vsdg/region.h>
-#include <jive/vsdg/cut-private.h>
+
+#include <jive/common.h>
+#include <jive/arch/registers.h>
+#include <jive/arch/stackframe.h>
+#include <jive/arch/transfer-instructions.h>
+#include <jive/regalloc/shaped-graph.h>
 #include <jive/vsdg/graph.h>
 #include <jive/vsdg/valuetype.h>
 #include <jive/vsdg/statetype.h>
 #include <jive/vsdg/node-private.h>
+#include <jive/vsdg/region.h>
 #include <jive/vsdg/traverser.h>
-#include <jive/arch/registers.h>
-#include <jive/arch/transfer-instructions.h>
-#include <jive/arch/stackframe.h>
+#include <jive/vsdg/variable.h>
 
 #include <string.h>
 
-static char *
-_jive_aux_valuecopy_node_get_label(const jive_node * self)
-{
-	return strdup("PSEUDO_COPY");
-}
 
 static void
-_jive_aux_valuecopy_node_init(jive_aux_valuecopy_node * self, jive_region * region, const jive_regcls * regcls, jive_output * origin)
+jive_aux_split_node_init_(
+	jive_aux_split_node * self,
+	jive_region * region,
+	const jive_type * in_type,
+	jive_output * in_origin,
+	const struct jive_resource_class * in_class,
+	const jive_type * out_type,
+	const struct jive_resource_class * out_class)
 {
-	const jive_type * type = jive_regcls_get_type(regcls);
-	self->base.class_ = &JIVE_AUX_VALUECOPY_NODE;
+	self->base.class_ = &JIVE_AUX_SPLIT_NODE;
 	_jive_node_init(&self->base, region,
-		1, &type, &origin,
-		1, &type);
+		1, &in_type, &in_origin,
+		1, &out_type);
 	
-	self->attrs.regcls = regcls;
-	((jive_value_input *)self->base.inputs[0])->required_regcls = self->attrs.regcls;
-	((jive_value_output *)self->base.outputs[0])->required_regcls = self->attrs.regcls;
-}
-
-const jive_node_attrs *
-_jive_aux_valuecopy_node_get_attrs(const jive_node * self_)
-{
-	const jive_aux_valuecopy_node * self = (const jive_aux_valuecopy_node *) self_;
-	return &self->attrs.base;
+	self->attrs.in_class = in_class;
+	self->attrs.out_class = out_class;
+	self->base.inputs[0]->required_rescls = in_class;
+	self->base.outputs[0]->required_rescls = out_class;
 }
 
 static jive_node *
-_jive_aux_valuecopy_node_create(jive_region * region, const jive_node_attrs * attrs_,
-	size_t noperands, jive_output * operands[])
+jive_aux_split_node_create_(jive_region * region, const jive_node_attrs * attrs_,
+	size_t noperands, jive_output * const operands[])
 {
-	const jive_aux_node_attrs * attrs = (const jive_aux_node_attrs *) attrs_;
-	return jive_aux_valuecopy_node_create(region, attrs->regcls, operands[0]);
+	/* FIXME: cannot (yet?) be copied */
+	abort();
+	#if 0
+	JIVE_DEBUG_ASSERT(noperands == 1);
+	const jive_aux_split_node_attrs * attrs = (const jive_aux_split_node_attrs *) attrs_;
+	return jive_aux_split_node_create(region,
+		attrs->in_class,
+		jive_input_get_type(self->inputs[0]),
+		operands[0],
+		attrs->out_class,
+		jive_output_get_type(self->outputs[0]));
+	#endif
+	return 0;
 }
 
-static bool
-_jive_aux_valuecopy_node_equiv(const jive_node_attrs * first_, const jive_node_attrs * second_)
-{
-	const jive_aux_node_attrs * first = (const jive_aux_node_attrs *) first_;
-	const jive_aux_node_attrs * second = (const jive_aux_node_attrs *) second_;
-	return first->regcls == second->regcls;
-}
-
-const jive_node_class JIVE_AUX_VALUECOPY_NODE = {
-	.parent = 0,
+const jive_node_class JIVE_AUX_SPLIT_NODE = {
+	.parent = &JIVE_NODE,
+	.name = "AUX_SPLIT",
 	.fini = _jive_node_fini, /* inherit */
-	.get_label = _jive_aux_valuecopy_node_get_label, /* override */
-	.get_attrs = _jive_aux_valuecopy_node_get_attrs, /* override */
-	.create = _jive_aux_valuecopy_node_create, /* override */
-	.equiv = _jive_aux_valuecopy_node_equiv, /* override */
-	.can_reduce = _jive_node_can_reduce, /* inherit */
-	.reduce = _jive_node_reduce, /* inherit */
-	.get_aux_regcls = _jive_node_get_aux_regcls /* inherit */
+	.get_label = _jive_node_get_label, /* inherit */
+	.get_attrs = _jive_node_get_attrs, /* inherit */
+	.match_attrs = _jive_node_match_attrs, /* inherit */
+	.create = jive_aux_split_node_create_, /* override */
+	.get_aux_rescls = _jive_node_get_aux_rescls /* inherit */
 };
+
+jive_node *
+jive_aux_split_node_create(jive_region * region,
+	const jive_type * in_type,
+	jive_output * in_origin,
+	const struct jive_resource_class * in_class,
+	const jive_type * out_type,
+	const struct jive_resource_class * out_class)
+{
+	jive_aux_split_node * self = jive_context_malloc(region->graph->context, sizeof(*self));
+	jive_aux_split_node_init_(self, region, in_type, in_origin, in_class, out_type, out_class);
+	
+	return &self->base;
+}
+
+static void
+replace_aux_split_node(jive_shaped_node * shaped_node, jive_node * node, const jive_transfer_instructions_factory * gen)
+{
+	jive_xfer_block xfer = gen->create_xfer(node->region, node->inputs[0]->origin,
+		node->inputs[0]->required_rescls, node->outputs[0]->required_rescls);
+	if (!xfer.input)
+		xfer.input = jive_node_add_input(xfer.node, jive_input_get_type(node->inputs[0]), node->inputs[0]->origin);
+	if (!xfer.output)
+		xfer.output = jive_node_add_output(xfer.node, jive_output_get_type(node->outputs[0]));
+	
+	jive_input_auto_assign_variable(xfer.input);
+	jive_ssavar * outvar = node->outputs[0]->ssavar;
+	jive_variable_merge(outvar->variable, jive_output_get_constraint(xfer.output));
+	jive_ssavar_divert_origin(outvar, xfer.output);
+	
+	jive_cut * cut = shaped_node->cut;
+	jive_shaped_node * pos = shaped_node->cut_location_list.next;
+	jive_shaped_node_destroy(shaped_node);
+	jive_cut_insert(cut, pos, xfer.node);
+	
+	jive_node_destroy(node);
+}
+
+void
+jive_regalloc_auxnodes_replace(jive_shaped_graph * shaped_graph, const jive_transfer_instructions_factory * gen)
+{
+	jive_traverser * traverser = jive_bottomup_traverser_create(shaped_graph->graph);
+	jive_node * node;
+	while( (node = jive_traverser_next(traverser)) != 0) {
+		if (jive_node_isinstance(node, &JIVE_AUX_SPLIT_NODE)) {
+			jive_shaped_node * shaped_node = jive_shaped_graph_map_node(shaped_graph, node);
+			replace_aux_split_node(shaped_node, node, gen);
+		}
+	}
+	jive_traverser_destroy(traverser);
+}
+
+#if 0
+
+
+
 
 jive_node *
 jive_aux_valuecopy_node_create(jive_region * region, const jive_regcls * regcls, jive_output * origin)
@@ -308,3 +364,5 @@ jive_regalloc_auxnodes_replace(jive_graph * graph, const jive_transfer_instructi
 {
 	replace_pseudo_nodes(graph, gen);
 }
+
+#endif
