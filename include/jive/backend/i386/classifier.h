ace3d3d941f6c3dea726befe769b1fd89838be02
Helge Bahmann <hcb@chaoticmind.net>
2011-04-19 22:03:04 +0200

    i386: placeholder register model classifier
    Classify all operations as "gpr" to satisfy interface requirements.

---
+++ include/jive/backend/i386/classifier.h
@@ -0,0 +1,8 @@
+#ifndef JIVE_BACKEND_I386_CLASSIFIER_H
+#define JIVE_BACKEND_I386_CLASSIFIER_H
+
+#include <jive/arch/regselector.h>
+
+extern const jive_reg_classifier jive_i386_reg_classifier;
+
+#endif
---
+++ src/backend/i386/classifier.c
@@ -0,0 +1,54 @@
+#include <jive/backend/i386/classifier.h>
+
+#include <jive/backend/i386/registerset.h>
+#include <jive/vsdg/basetype.h>
+
+static jive_regselect_mask
+jive_i386_classify_type_(const jive_type * type, const jive_resource_class * rescls)
+{
+	while (rescls->parent != &jive_root_resource_class)
+		rescls = rescls->parent;
+	if (rescls == &jive_i386_regcls[jive_i386_gpr].base)
+		return (1 << jive_i386_gpr);
+	else if (rescls == &jive_i386_regcls[jive_i386_flags].base)
+		return (1 << jive_i386_flags);
+	
+	return 0;
+}
+
+static jive_regselect_mask
+jive_i386_classify_fixed_arithmetic_(jive_bitop_code op, size_t nbits)
+{
+	return (1 << jive_i386_gpr);
+}
+
+static jive_regselect_mask
+jive_i386_classify_fixed_compare_(jive_bitop_code op, size_t nbits)
+{
+	return (1 << jive_i386_gpr);
+}
+
+static jive_regselect_mask
+jive_i386_classify_address_(void)
+{
+	return (1 << jive_i386_gpr);
+}
+
+static const jive_register_class * classes [] = 
+{
+	[jive_i386_gpr] = &jive_i386_regcls[jive_i386_gpr],
+	[jive_i386_fp] = &jive_i386_regcls[jive_i386_fp],
+	[jive_i386_mmx] = &jive_i386_regcls[jive_i386_mmx],
+	[jive_i386_sse] = &jive_i386_regcls[jive_i386_sse],
+	[jive_i386_flags] = &jive_i386_regcls[jive_i386_flags],
+};
+
+const jive_reg_classifier jive_i386_reg_classifier = {
+	.any = (1 << jive_i386_gpr) | (1 << jive_i386_flags),
+	.classify_type = jive_i386_classify_type_,
+	.classify_fixed_arithmetic = jive_i386_classify_fixed_arithmetic_,
+	.classify_fixed_compare = jive_i386_classify_fixed_compare_,
+	.classify_address = jive_i386_classify_address_,
+	
+	.classes = classes,
+};
