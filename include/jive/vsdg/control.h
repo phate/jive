9d0261cd750d8eccfa55f145f27042e546878b13
Helge Bahmann <hcb@chaoticmind.net>
2011-04-24 21:11:23 +0200

    vsdg: "choice" operator
    Adapt gamma ("choice") operator to changed API.

--- include/jive/vsdg/control.h
+++ include/jive/vsdg/control.h
@@ -1,18 +1,32 @@
 #ifndef JIVE_VSDG_CONTROL_H
 #define JIVE_VSDG_CONTROL_H
 
+#include <jive/vsdg/controltype.h>
 #include <jive/vsdg/node.h>
 
-extern const jive_node_class JIVE_GAMMA_SLAVE_NODE;
-extern const jive_node_class JIVE_GAMMA_MASTER_NODE;
+struct jive_output;
+struct jive_type;
+struct jive_region;
+
+extern const jive_node_class JIVE_GAMMA_TAIL_NODE;
+extern const jive_node_class JIVE_GAMMA_NODE;
 
 extern const jive_node_class JIVE_THETA_HEAD_NODE;
 extern const jive_node_class JIVE_THETA_TAIL_NODE;
 extern const jive_node_class JIVE_THETA_NODE;
 
-typedef jive_node jive_abstract_gamma_slave_node;
-typedef jive_node jive_abstract_gamma_master_node;
-typedef jive_node jive_gamma_slave_node;
-typedef jive_node jive_gamma_master_node;
+struct jive_output * const *
+jive_choose(struct jive_output * predicate,
+	size_t nvalues, const struct jive_type * types[const],
+	struct jive_output * false_values[const],
+	struct jive_output * true_values[const]);
+
+struct jive_node *
+jive_gamma_create(
+	struct jive_region * region,
+	struct jive_output * predicate,
+	size_t nvalues, const struct jive_type * types[const],
+	struct jive_output * false_values[const],
+	struct jive_output * true_values[const]);
 
 #endif
--- src/vsdg/control.c
+++ src/vsdg/control.c
@@ -1,147 +1,111 @@
-//#include <jive/vsdg/control-private.h>
-#include <jive/vsdg/node-private.h>
+#include <jive/vsdg/control.h>
 
 #include <string.h>
+#include <stdio.h>
 
-const jive_node_class JIVE_GAMMA_SLAVE_NODE;
-const jive_node_class JIVE_GAMMA_MASTER_NODE;
+#include <jive/context.h>
+#include <jive/vsdg/anchortype.h>
+#include <jive/vsdg/graph.h>
+#include <jive/vsdg/node-private.h>
+#include <jive/vsdg/region.h>
+
+const jive_node_class JIVE_GAMMA_TAIL_NODE = {
+	.parent = &JIVE_NODE,
+	.name = "GAMMA_TAIL",
+	.fini = _jive_node_fini,  /* inherit */
+	.get_default_normal_form = _jive_node_get_default_normal_form,  /* inherit */
+	.get_label = _jive_node_get_label,  /* inherit */
+	.get_attrs = _jive_node_get_attrs,  /* inherit */
+	.match_attrs = _jive_node_match_attrs,  /* inherit */
+	.create = _jive_node_create,  /* inherit */
+	.get_aux_rescls = _jive_node_get_aux_rescls  /* inherit */
+};
+
+const jive_node_class JIVE_GAMMA_NODE = {
+	.parent = &JIVE_NODE,
+	.name = "GAMMA",
+	.fini = _jive_node_fini,  /* inherit */
+	.get_default_normal_form = _jive_node_get_default_normal_form,  /* inherit */
+	.get_label = _jive_node_get_label,  /* inherit */
+	.get_attrs = _jive_node_get_attrs,  /* inherit */
+	.match_attrs = _jive_node_match_attrs,  /* inherit */
+	.create = _jive_node_create,  /* inherit */
+	.get_aux_rescls = _jive_node_get_aux_rescls  /* inherit */
+};
+
+static jive_node *
+jive_gamma_tail_node_create(jive_region * region)
+{
+	jive_node * self = jive_context_malloc(region->graph->context, sizeof(*self));
+	JIVE_DECLARE_ANCHOR_TYPE(anchor);
+	self->class_ = &JIVE_GAMMA_TAIL_NODE;
+	_jive_node_init(self, region,
+		0, NULL, NULL,
+		1, &anchor);
+	
+	region->bottom = self;
+	return self;
+}
+
+static jive_node *
+jive_gamma_node_create(jive_region * region,
+	jive_output * predicate,
+	jive_output * false_alternative,
+	jive_output * true_alternative)
+{
+	jive_node * self = jive_context_malloc(region->graph->context, sizeof(*self));;
+	JIVE_DECLARE_ANCHOR_TYPE(anchor);
+	JIVE_DECLARE_CONTROL_TYPE(ctl);
+	self->class_ = &JIVE_GAMMA_NODE;
+	_jive_node_init(self, region,
+		3, (const jive_type *[]){anchor, anchor, ctl}, (jive_output *[]){false_alternative, true_alternative, predicate},
+		0, NULL);
+	
+	return self;
+}
+
+jive_node *
+jive_gamma_create(
+	jive_region * region,
+	jive_output * predicate,
+	size_t nvalues, const jive_type * types[const],
+	jive_output * false_values[const],
+	jive_output * true_values[const])
+{
+	jive_region * false_region = jive_region_create_subregion(region);
+	jive_region * true_region = jive_region_create_subregion(region);
+	jive_node * false_alt = jive_gamma_tail_node_create(false_region);
+	jive_node * true_alt = jive_gamma_tail_node_create(true_region);
+	jive_node * gamma = jive_gamma_node_create(region, predicate, false_alt->outputs[0], true_alt->outputs[0]);
+	
+	size_t n;
+	for (n = 0; n < nvalues; n++) {
+		char name[80];
+		snprintf(name, sizeof(name), "gamma_%08x_%d", (intptr_t)gamma, n);
+		jive_gate * gate = jive_type_create_gate(types[n], region->graph, name);
+		jive_node_gate_input(false_alt, gate, false_values[n]);
+		jive_node_gate_input(true_alt, gate, true_values[n]);
+		jive_node_gate_output(gamma, gate);
+	}
+	return gamma;
+}
+
+jive_output * const *
+jive_choose(jive_output * predicate,
+	size_t nvalues, const jive_type * types[const],
+	jive_output * false_values[const],
+	jive_output * true_values[const])
+{
+	jive_output * tmp[nvalues * 2 + 1];
+	size_t n;
+	tmp[0] = predicate;
+	for(n = 0; n < nvalues; n++) tmp[n + 1] = false_values[n];
+	for(n = 0; n < nvalues; n++) tmp[n + nvalues + 1] = true_values[n];
+	jive_region * region = jive_region_innermost(nvalues * 2 + 1, tmp);
+	
+	return jive_gamma_create(region, predicate, nvalues, types, false_values, true_values)->outputs;
+}
 
 const jive_node_class JIVE_THETA_HEAD_NODE;
 const jive_node_class JIVE_THETA_TAIL_NODE;
 const jive_node_class JIVE_THETA_NODE;
-
-#if 0
-
-const jive_node_class JIVE_ABSTRACT_GAMMA_SLAVE_NODE = {
-	.parent = 0,
-	.fini = _jive_node_fini, /* inherit */
-	.get_label = _jive_node_get_label, /* inherit */
-	.get_attrs = _jive_node_get_attrs, /* inherit */
-	.create = _jive_node_create, /* inherit */
-	.equiv = _jive_node_equiv, /* inherit */
-	.can_reduce = _jive_node_can_reduce, /* inherit */
-	.reduce = _jive_node_reduce, /* inherit */
-	.get_aux_regcls = _jive_node_get_aux_regcls /* inherit */
-};
-
-void
-_jive_abstract_gamma_slave_node_init(
-	jive_abstract_gamma_slave_node * self,
-	struct jive_region * region,
-	size_t noperands,
-	const struct jive_type * operand_types[const],
-	struct jive_output * operands[const],
-	size_t noutputs,
-	const struct jive_type * output_types[const])
-{
-	size_t real_noutputs = noutputs + 1, n;
-	const jive_type * real_output_types[real_noutputs];
-	JIVE_DECLARE_CONTROL_TYPE(ctrl);
-	real_output_types[0] = ctrl;
-	for(n=0; n<noutputs; n++) real_output_types[n+1] = output_types[n];
-	
-	_jive_node_init(self, region, noperands, operand_types, operands, real_noutputs, real_output_types);
-}
-
-const jive_node_class JIVE_ABSTRACT_GAMMA_MASTER_NODE = {
-	.parent = 0,
-	.fini = _jive_node_fini, /* inherit */
-	.get_label = _jive_node_get_label, /* inherit */
-	.get_attrs = _jive_node_get_attrs, /* inherit */
-	.create = _jive_node_create, /* inherit */
-	.equiv = _jive_node_equiv, /* inherit */
-	.can_reduce = _jive_node_can_reduce, /* inherit */
-	.reduce = _jive_node_reduce, /* inherit */
-	.get_aux_regcls = _jive_node_get_aux_regcls /* inherit */
-};
-
-void
-_jive_abstract_gamma_master_node_init(
-	jive_abstract_gamma_slave_node * self,
-	struct jive_region * region,
-	jive_control_output * false_alternative,
-	jive_control_output * true_alternative,
-	size_t noperands,
-	const struct jive_type * operand_types[const],
-	struct jive_output * operands[const],
-	size_t noutputs,
-	const struct jive_type * output_types[const])
-{
-	size_t real_noperands = noperands + 2, n;
-	const jive_type * real_operand_types[real_noperands];
-	jive_output * real_operands[real_noperands];
-	JIVE_DECLARE_CONTROL_TYPE(ctrl);
-	real_operand_types[0] = ctrl; real_operands[0] = (jive_output *)false_alternative;
-	real_operand_types[1] = ctrl; real_operands[1] = (jive_output *)true_alternative;
-	for(n=0; n<real_noperands; n++) {
-		real_operand_types[n+2] = operand_types[n];
-		real_operands[n+2] = operands[n];
-	}
-	
-	_jive_node_init(self, region, real_noperands, real_operand_types, real_operands, noutputs, output_types);
-}
-
-const jive_node_class JIVE_GAMMA_SLAVE_NODE = {
-	.parent = 0,
-	.fini = _jive_node_fini, /* inherit */
-	.get_label = _jive_gamma_slave_node_get_label, /* override */
-	.get_attrs = _jive_node_get_attrs, /* inherit */
-	.create = _jive_node_create, /* inherit */ /* FIXME: override */
-	.equiv = _jive_node_equiv, /* inherit */
-	.can_reduce = _jive_node_can_reduce, /* inherit */
-	.reduce = _jive_node_reduce, /* inherit */
-	.get_aux_regcls = _jive_node_get_aux_regcls /* inherit */
-};
-
-void
-_jive_gamma_slave_node_init(
-	jive_gamma_slave_node * self,
-	struct jive_region * region)
-{
-	_jive_abstract_gamma_slave_node_init(self, region,
-		0, 0, 0,
-		0, 0);
-}
-
-char *
-_jive_gamma_slave_node_get_label(const jive_node * self)
-{
-	return strdup("ALTERNATIVE");
-}
-
-const jive_node_class JIVE__GAMMA_MASTER_NODE = {
-	.parent = 0,
-	.fini = _jive_node_fini, /* inherit */
-	.get_label = _jive_gamma_slave_node_get_label, /* override */
-	.get_attrs = _jive_node_get_attrs, /* inherit */
-	.create = _jive_node_create, /* inherit */ /* FIXME: override */
-	.equiv = _jive_node_equiv, /* inherit */
-	.can_reduce = _jive_node_can_reduce, /* inherit */
-	.reduce = _jive_node_reduce, /* inherit */
-	.get_aux_regcls = _jive_node_get_aux_regcls /* inherit */
-};
-
-void
-_jive_gamma_master_node_init(
-	jive_gamma_slave_node * self,
-	struct jive_region * region,
-	jive_control_output * false_alternative,
-	jive_control_output * true_alternative,
-	jive_value * predicate)
-{
-	jive_output * operand = (jive_output *) predicate;
-	JIVE_DECLARE_VALUE_TYPE(value_type);
-	
-	_jive_abstract_gamma_master_node_init(self, region,
-		false_alternative, true_alternative,
-		1, &value_type, &operand,
-		0, 0);
-}
-
-char *
-_jive_gamma_master_node_get_label(const jive_node * self)
-{
-	return strdup("GAMMA");
-}
-
-#endif
