74079999a73f9b115c653aa1dbda0fcfcbc84243
Helge Bahmann <hcb@chaoticmind.net>
2011-07-21 14:22:46 +0200

    vsdg: extend functionality of debug dump helpers
    Extend functionality of helpers doing debug dumps of data
    structures (to be used through gdb interactively).

---
+++ src/regalloc/debug-dump.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+#include <jive/regalloc/shaped-variable-private.h>
+#include <jive/regalloc/xpoint-private.h>
+#include <jive/vsdg.h>
+
+void
+jive_shaped_variable_dump(const jive_shaped_variable * self)
+{
+	fprintf(stderr, "var %p: ", self->variable);
+	struct jive_variable_interference_hash_iterator i;
+	JIVE_HASH_ITERATE(jive_variable_interference_hash, self->interference, i) {
+		jive_shaped_variable * other = i.entry->shaped_variable;
+		fprintf(stderr, "(%p; %p:%zd) ", other->variable, other->variable->ssavars.first->origin->node, other->variable->ssavars.first->origin->index);
+	}
+	fprintf(stderr, " use_count=%zd\n", self->variable->use_count);
+}
+
+
+void
+jive_shaped_node_dump(const jive_shaped_node * shaped_node)
+{
+	struct jive_nodevar_xpoint_hash_byssavar_iterator i;
+	
+	JIVE_HASH_ITERATE(jive_nodevar_xpoint_hash_byssavar, shaped_node->ssavar_xpoints, i) {
+		jive_nodevar_xpoint * xpoint = i.entry;
+		if (!xpoint->before_count)
+			continue;
+		jive_ssavar * ssavar = xpoint->shaped_ssavar->ssavar;
+		fprintf(stderr, "(%p,%p,%p:%zd) ", ssavar, ssavar->variable, ssavar->origin->node, ssavar->origin->index);
+	}
+	fprintf(stderr, "\nNODE %p\n", shaped_node->node);
+	JIVE_HASH_ITERATE(jive_nodevar_xpoint_hash_byssavar, shaped_node->ssavar_xpoints, i) {
+		jive_nodevar_xpoint * xpoint = i.entry;
+		if (!xpoint->after_count)
+			continue;
+		jive_ssavar * ssavar = xpoint->shaped_ssavar->ssavar;
+		fprintf(stderr, "(%p,%p,%p:%zd) ", ssavar, ssavar->variable, ssavar->origin->node, ssavar->origin->index);
+	}
+	fprintf(stderr, "\n");
+}
+
+void
+jive_shaped_region_dump(jive_shaped_graph * shaped_graph, jive_region * region)
+{
+	jive_region * subregion;
+	JIVE_LIST_ITERATE(region->subregions, subregion, region_subregions_list)
+		jive_shaped_region_dump(shaped_graph, subregion);
+	
+	const jive_shaped_region * shaped_region = jive_shaped_graph_map_region(shaped_graph, region);
+	
+	fprintf(stderr, "region %p\n", shaped_region->region);
+	jive_cut * cut;
+	JIVE_LIST_ITERATE(shaped_region->cuts, cut, region_cut_list) {
+		jive_shaped_node * shaped_node;
+		fprintf(stderr, "%p:", cut);
+		JIVE_LIST_ITERATE(cut->locations, shaped_node, cut_location_list)
+			fprintf(stderr, " %p", shaped_node->node);
+		fprintf(stderr, "\n");
+	}
+}
+
+void
+jive_shaped_graph_dump(jive_shaped_graph * shaped_graph)
+{
+	jive_shaped_region_dump(shaped_graph, shaped_graph->graph->root_region);
+}
---
+++ src/vsdg/debug-dump.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <jive/vsdg/basetype.h>
+#include <jive/vsdg/resource-private.h>
+#include <jive/vsdg/variable.h>
+
+void
+jive_resource_class_count_dump(const jive_resource_class_count * count)
+{
+	jive_resource_class_count_item * item;
+	JIVE_LIST_ITERATE(count->items, item, item_list) {
+		fprintf(stderr, "%s:%zd ", item->resource_class->name, item->count);
+	}
+	fprintf(stderr, "\n");
+}
+
+void
+jive_variable_dump(const jive_variable * self)
+{
+	jive_ssavar * ssavar;
+	fprintf(stderr, "var %p: ", self);
+	JIVE_LIST_ITERATE(self->ssavars, ssavar, variable_ssavar_list)
+		fprintf(stderr, "%p:%zd ", ssavar->origin->node, ssavar->origin->index);
+	fprintf(stderr, " use_count=%zd\n", self->use_count);
+}
