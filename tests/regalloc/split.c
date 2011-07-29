fe46fe22657a75642659c14ab1bc7dc0f479bac4
Helge Bahmann <hcb@chaoticmind.net>
2011-07-29 10:47:05 +0200

    regalloc: extend lifetime split unit test
    Add another unit test to exercise lifetime splitting, this time
    to make sure that assignment of the last possible register to one
    variable at its definition site can cause a forced split of a
    different variable.

--- tests/regalloc/split.c
+++ tests/regalloc/split.c
@@ -72,11 +72,81 @@ create_testgraph_split(jive_context * context)
 	return graph;
 }
 
+static jive_graph *
+create_testgraph_emerg_split(jive_context * context)
+{
+	/* creates variables with the following lifetimes:
+	 a   c
+	 |   |
+	 |     d
+	 |     |
+	 |       e
+	 |       |
+	   b     |
+	   |     |
+	   |       f
+	   |       |
+	
+	with the following register constraints:
+	a - r1
+	b - r2
+	c,d,e,f - each either r1 or r2
+	
+	Due to number of intefering values, a will be assigned
+	r1 first (denying r1 to e), and subsequently b will
+	be considered. Even after life range split, assigning
+	the only possible choice r2 to b, it leaves no register
+	for e over its lifetime.
+	
+	The register allocator must allow r2 for b after splitting
+	the lifetime such that no use of e falls within its
+	lifetime. */
+	
+	jive_graph * graph;
+	jive_node * enter, * leave;
+	proc_frame(context, &graph, &enter, &leave);
+	
+	jive_gate * arg3_gate = jive_register_class_create_gate(&jive_testarch_regcls[cls_r3], graph, "arg2");
+	jive_output * arg3 = jive_node_gate_output(enter, arg3_gate);
+	jive_node_gate_input(leave, arg3_gate, arg3);
+	
+	jive_gate * cls2_gate = jive_register_class_create_gate(&jive_testarch_regcls[cls_r2], graph, "cls2");
+	jive_output * arg1 = jive_node_gate_output(enter, cls2_gate);
+	jive_node * op3 = jive_instruction_node_create(
+		graph->root_region,
+		&jive_testarch_instructions[setr1_index],
+		(jive_output *[]) {arg1}, NULL);
+	jive_node_gate_input(leave, jive_register_class_create_gate(&jive_testarch_regcls[cls_r1], graph, "cls1"), op3->outputs[0]);
+	
+	jive_gate * arg2_gate = jive_register_class_create_gate(&jive_testarch_regcls[cls_gpr], graph, "1or2");
+	jive_output * arg2 = jive_node_gate_output(enter, arg2_gate);
+	jive_node * op1 = jive_instruction_node_create(
+		graph->root_region,
+		&jive_testarch_instructions[move_gpr_index],
+		(jive_output *[]) {arg2}, NULL);
+	jive_node * op2 = jive_instruction_node_create(
+		graph->root_region,
+		&jive_testarch_instructions[move_gpr_index],
+		(jive_output *[]) {op1->outputs[0]}, NULL);
+	jive_node * op4 = jive_instruction_node_create(
+		graph->root_region,
+		&jive_testarch_instructions[move_gpr_index],
+		(jive_output *[]) {op2->outputs[0]}, NULL);
+	jive_node_gate_input(leave, jive_register_class_create_gate(&jive_testarch_regcls[cls_gpr], graph, "cls1"), op4->outputs[0]);
+	
+	JIVE_DECLARE_STATE_TYPE(state_type);
+	
+	jive_node_add_input(op3, state_type, jive_node_add_output(op2, state_type));
+	jive_node_add_input(op4, state_type, jive_node_add_output(op3, state_type));
+	
+	return graph;
+}
 
 typedef jive_graph * (*creator_function_t)(jive_context *);
 
 static const creator_function_t tests[] = {
 	create_testgraph_split,
+	create_testgraph_emerg_split
 };
 
 int main()
