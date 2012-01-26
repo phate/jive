0cef65248fc58a8be1f5514420b15770846c6627
Nico Reißmann <nico.reissmann@gmail.com>
2012-01-26 23:14:11 +0100

    bitstring: add missing includes bitstring.h
    Add missing things to types/bitstring.h and keep include list
    sorted alphabetically.
    
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- include/jive/types/bitstring.h
+++ include/jive/types/bitstring.h
@@ -1,12 +1,16 @@
 #ifndef JIVE_TYPES_BITSTRING_H
 #define JIVE_TYPES_BITSTRING_H
 
-#include <jive/types/bitstring/type.h>
-#include <jive/types/bitstring/constant.h>
-#include <jive/types/bitstring/symbolic-constant.h>
-#include <jive/types/bitstring/slice.h>
-#include <jive/types/bitstring/concat.h>
 #include <jive/types/bitstring/arithmetic.h>
+#include <jive/types/bitstring/bitoperation-classes.h>
+#include <jive/types/bitstring/bitstring-operations.h>
+#include <jive/types/bitstring/concat.h>
+#include <jive/types/bitstring/constant.h>
 #include <jive/types/bitstring/comparison.h>
+#include <jive/types/bitstring/slice.h>
+#include <jive/types/bitstring/symbolic-constant.h>
+#include <jive/types/bitstring/symbolic-expression.h>
+#include <jive/types/bitstring/type.h>
+#include <jive/types/bitstring/type-private.h>
 
 #endif
