b9b1400a1bbe4e2544688b0c4e97bacdc5b43f1a
Nico Reißmann <nico.reissmann@gmail.com>
2012-08-04 15:27:09 +0200

    types: added record.h to types
    Allow to include all record-related headers through the catch-all
    type header.
    
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

---
+++ include/jive/types/record.h
@@ -0,0 +1,8 @@
+#ifndef JIVE_TYPES_RECORD_H
+#define JIVE_TYPES_RECORD_H
+
+#include <jive/types/record/rcdtype.h>
+#include <jive/types/record/rcdgroup.h>
+#include <jive/types/record/rcdselect.h>
+
+#endif
