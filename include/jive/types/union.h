f4a9443d9f25af07bf25957a97801b091557ed77
Nico Reißmann <nico.reissmann@gmail.com>
2012-08-08 20:58:36 +0200

    types: added union.h to types
    Allow to include all union-related headers through the catch-all
    type header.
    
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

---
+++ include/jive/types/union.h
@@ -0,0 +1,8 @@
+#ifndef JIVE_TYPES_UNION_H
+#define JIVE_TYPES_UNION_H
+
+#include <jive/types/union/unntype.h>
+#include <jive/types/union/unnunify.h>
+#include <jive/types/union/unnchoose.h>
+
+#endif
