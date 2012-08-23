486208b30a65c837f730f2e559fac5a4906d39e8
Nico Reißmann <nico.reissmann@gmail.com>
2012-08-23 21:16:52 +0200

    arch: unit test for load
    Test that basic instantiation of loads works as expected; also verify that
    load after store reduction works as expected.
    
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

---
+++ tests/arch/test-load.c
@@ -0,0 +1,58 @@
+#include "test-registry.h"
+
+#include <assert.h>
+#include <locale.h>
+
+#include <jive/view.h>
+#include <jive/vsdg.h>
+#include <jive/arch/load.h>
+#include <jive/arch/store.h>
+#include <jive/arch/memory.h>
+#include <jive/arch/addresstype.h>
+#include <jive/types/bitstring/type.h>
+#include <jive/vsdg/node-private.h>
+
+static int test_main()
+{
+	setlocale(LC_ALL, "");
+
+	jive_context * context = jive_context_create();
+	jive_graph * graph = jive_graph_create(context);
+
+	JIVE_DECLARE_MEMORY_TYPE(memtype);
+	JIVE_DECLARE_ADDRESS_TYPE(addrtype);
+	JIVE_DECLARE_BITSTRING_TYPE(bits32, 32);
+	jive_node * top = jive_node_create(graph->root_region,
+		0, NULL, NULL,
+		4, (const jive_type *[]){addrtype, addrtype, memtype, bits32});
+
+	jive_output * load0 = jive_load_by_address_create(top->outputs[0],
+		jive_value_type_cast(bits32), 1, &top->outputs[2]);
+
+	jive_output * state;
+	jive_store_by_address_create(top->outputs[1], jive_value_type_cast(bits32),
+		top->outputs[3], 1, &top->outputs[2], &state);
+	jive_output * load1 = jive_load_by_address_create(top->outputs[1],
+		jive_value_type_cast(bits32), 1, &state);
+	assert(load1 == top->outputs[3]);
+
+	jive_node * bottom = jive_node_create(graph->root_region,
+		2, (const jive_type *[]){bits32, bits32}, (jive_output *[]){load0, load1},
+		0, NULL);
+	jive_node_reserve(bottom);
+
+	jive_graph_normalize(graph);
+	jive_graph_prune(graph);
+
+	jive_view(graph, stderr);
+
+	assert(bottom->inputs[1]->origin->node == top);
+
+	jive_graph_destroy(graph);
+	assert(jive_context_is_empty(context));
+	jive_context_destroy(context);
+
+	return 0;
+}
+
+JIVE_UNIT_TEST_REGISTER("arch/test-load", test_main);
