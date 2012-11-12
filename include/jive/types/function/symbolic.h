2d2e8260b8a5208e9cf04ebc7cddc5380be09fb9
Helge Bahmann <hcb@chaoticmind.net>
2012-11-12 19:18:57 +0100

    function: add missing symolic.c
    Re-add symbolic function implementation that accidentally got dropped.

---
+++ include/jive/types/function/symbolic.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2010 2011 2012 Helge Bahmann <hcb@chaoticmind.net>
+ * Copyright 2011 2012 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#ifndef JIVE_TYPES_FUNCTION_SYMBOLIC_H
+#define JIVE_TYPES_FUNCTION_SYMBOLIC_H
+
+#include <jive/vsdg/node.h>
+#include <jive/types/function/fcttype.h>
+
+extern const jive_node_class JIVE_SYMBOLICFUNCTION_NODE;
+
+typedef struct jive_symbolicfunction_node jive_symbolicfunction_node;
+typedef struct jive_symbolicfunction_node_attrs jive_symbolicfunction_node_attrs;
+
+struct jive_symbolicfunction_node_attrs {
+	jive_node_attrs base;
+	const char * name;
+	jive_function_type type;
+};
+
+struct jive_symbolicfunction_node {
+	jive_node base;
+	jive_symbolicfunction_node_attrs attrs; 
+};
+
+jive_node *
+jive_symbolicfunction_node_create(struct jive_graph * graph, const char * name, const jive_function_type * type);
+
+jive_output *
+jive_symbolicfunction_create(struct jive_graph * graph, const char * name, const jive_function_type * type);
+
+JIVE_EXPORTED_INLINE jive_symbolicfunction_node *
+jive_symbolicfunction_node_cast(jive_node * node)
+{
+	if(node->class_ == &JIVE_SYMBOLICFUNCTION_NODE) return (jive_symbolicfunction_node *) node;
+	else return 0;
+}
+
+#endif
---
+++ src/types/function/symbolic.c
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2010 2011 2012 Helge Bahmann <hcb@chaoticmind.net>
+ * Copyright 2011 2012 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include <jive/types/function/symbolic.h>
+
+#include <string.h>
+
+#include <jive/vsdg/controltype.h>
+#include <jive/vsdg/node-private.h>
+#include <jive/vsdg/anchortype.h>
+#include <jive/vsdg/substitution.h>
+
+static void
+jive_symbolicfunction_node_fini_(jive_node * self_);
+
+static char *
+jive_symbolicfunction_node_get_label_(const jive_node * self_);
+
+static const jive_node_attrs *
+jive_symbolicfunction_node_get_attrs_(const jive_node * self);
+
+static bool
+jive_symbolicfunction_node_match_attrs_(const jive_node * self, const jive_node_attrs * attrs);
+
+static jive_node *
+jive_symbolicfunction_node_create_(struct jive_region * region, const jive_node_attrs * attrs_,
+	size_t noperands, struct jive_output * const operands[]);
+
+const jive_node_class JIVE_SYMBOLICFUNCTION_NODE = {
+	.parent = &JIVE_NODE,
+	.name = "SYMBOLICFUNCTION",
+	.fini = jive_symbolicfunction_node_fini_, /* override */
+	.get_default_normal_form = jive_node_get_default_normal_form_, /* inherit */
+	.get_label = jive_symbolicfunction_node_get_label_, /* override */
+	.get_attrs = jive_symbolicfunction_node_get_attrs_, /* inherit */
+	.match_attrs = jive_symbolicfunction_node_match_attrs_, /* override */
+	.create = jive_symbolicfunction_node_create_, /* override */
+	.get_aux_rescls = jive_node_get_aux_rescls_ /* inherit */
+};
+
+static void
+jive_symbolicfunction_node_init_(
+	jive_symbolicfunction_node * node,
+	jive_graph * graph,
+	const char * fctname,
+	const jive_function_type * type)
+{
+	node->attrs.name = jive_context_strdup(graph->context, fctname);
+	jive_function_type_init(&node->attrs.type, graph->context,
+		type->narguments, (const jive_type **) type->argument_types,
+		type->nreturns, (const jive_type **) type->return_types);
+
+	const jive_type * rtype = &type->base.base;
+	jive_node_init_(&node->base, graph->root_region,
+		0, NULL, NULL,
+		1, &rtype);
+}
+
+static void
+jive_symbolicfunction_node_fini_(jive_node * self_)
+{
+	jive_symbolicfunction_node * self = (jive_symbolicfunction_node *) self_;
+	
+	jive_context_free(self_->graph->context, (char *)self->attrs.name);
+	
+	jive_function_type_fini(&self->attrs.type);
+	
+	jive_node_fini_(&self->base);
+}
+
+static char *
+jive_symbolicfunction_node_get_label_(const jive_node * self_)
+{
+	const jive_symbolicfunction_node * self = (const jive_symbolicfunction_node *) self_;
+	
+	return strdup(self->attrs.name);
+}
+
+static jive_node *
+jive_symbolicfunction_node_create_(struct jive_region * region, const jive_node_attrs * attrs_,
+	size_t noperands, struct jive_output * const operands[])
+{
+	const jive_symbolicfunction_node_attrs * attrs = (const jive_symbolicfunction_node_attrs *) attrs_;
+	return jive_symbolicfunction_node_create(region->graph, attrs->name, &attrs->type);
+}
+
+static const jive_node_attrs *
+jive_symbolicfunction_node_get_attrs_(const jive_node * self_)
+{
+	const jive_symbolicfunction_node * self = (const jive_symbolicfunction_node *) self_;
+	
+	return &self->attrs.base;
+}
+
+static bool
+jive_symbolicfunction_node_match_attrs_(const jive_node * self, const jive_node_attrs * attrs)
+{
+	const jive_symbolicfunction_node_attrs * first = &((const jive_symbolicfunction_node *)self)->attrs;
+	const jive_symbolicfunction_node_attrs * second = (const jive_symbolicfunction_node_attrs *) attrs;
+
+	if (!jive_type_equals(&first->type.base.base, &second->type.base.base)) return false;
+	if (strcmp(first->name, second->name)) return false;
+
+	return true;
+}
+
+jive_node *
+jive_symbolicfunction_node_create(struct jive_graph * graph, const char * name, const jive_function_type * type) 
+{
+	jive_symbolicfunction_node * node = jive_context_malloc(graph->context, sizeof(* node));
+	node->base.class_ = &JIVE_SYMBOLICFUNCTION_NODE;
+	jive_symbolicfunction_node_init_(node, graph, name, type);
+	return &node->base;
+} 
+
+jive_output *
+jive_symbolicfunction_create(struct jive_graph * graph, const char * name, const jive_function_type * type)
+{
+	return jive_symbolicfunction_node_create(graph, name, type)->outputs[0];
+}
