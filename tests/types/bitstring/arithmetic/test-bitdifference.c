b185238a3888e3312b28360f0ceeb3dd640e8579
Nico Reißmann <nico.reissmann@gmail.com>
2013-02-23 01:42:15 +0100

    tests: add test for bitdifference operator
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

---
+++ tests/types/bitstring/arithmetic/test-bitdifference.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include "test-registry.h"
+
+#include <locale.h>
+#include <assert.h>
+
+#include <jive/view.h>
+#include <jive/types/bitstring.h>
+#include <jive/vsdg/node-private.h>
+
+static int test_main(void)
+{
+	setlocale(LC_ALL, "");
+
+	jive_context * context = jive_context_create();
+	jive_graph * graph = jive_graph_create(context);
+
+	JIVE_DECLARE_BITSTRING_TYPE(bits32, 32);
+	jive_node * top = jive_node_create(graph->root_region,
+		0, NULL, NULL,
+		2, (const jive_type *[]){bits32, bits32});
+	jive_node_reserve(top);
+
+	jive_output * diff = jive_bitdifference(top->outputs[0], top->outputs[1]);
+
+	jive_node * bottom = jive_node_create(graph->root_region,
+		1, &bits32, &diff, 0, NULL);
+	jive_node_reserve(bottom);
+
+	jive_graph_normalize(graph);
+	jive_graph_prune(graph);
+	jive_view(graph, stdout);
+
+	assert(jive_node_isinstance(bottom->inputs[0]->origin->node, &JIVE_BITDIFFERENCE_NODE));
+
+	jive_graph_destroy(graph);
+	jive_context_assert_clean(context);
+	jive_context_destroy(context);
+
+	return 0;
+}
+
+JIVE_UNIT_TEST_REGISTER("types/bitstring/arithmetic/test-bitdifference", test_main);
