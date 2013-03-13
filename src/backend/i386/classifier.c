d960a8848a08793c3f29c99a11248688fe8ae213
Nico Reißmann <nico.reissmann@gmail.com>
2013-03-13 23:16:56 +0100

    i386: add support for floats to classifier
    Add support for single precision floating point operations to
    i386 register classifier.
    
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- src/backend/i386/classifier.c
+++ src/backend/i386/classifier.c
@@ -1,11 +1,13 @@
 /*
  * Copyright 2010 2011 2012 Helge Bahmann <hcb@chaoticmind.net>
+ * Copyright 2013 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
 #include <jive/backend/i386/classifier.h>
 
 #include <jive/backend/i386/registerset.h>
+#include <jive/types/float/flttype.h>
 #include <jive/vsdg/basetype.h>
 
 typedef enum jive_i386_classify_regcls {
@@ -37,6 +39,10 @@ jive_i386_classify_type_(const jive_type * type, const jive_resource_class * res
 		if (btype->nbits == 32)
 			return (1 << jive_i386_classify_gpr);
 	}
+
+	if (type->class_ == &JIVE_FLOAT_TYPE) {
+		return (1 << jive_i386_classify_sse);
+	}
 	
 	/* no suitable register class */
 	/* FIXME: this should *probably* not be a fatal error -- but since
@@ -59,6 +65,18 @@ jive_i386_classify_fixed_compare_(jive_bitcmp_code op, size_t nbits)
 }
 
 static jive_regselect_mask
+jive_i386_classify_float_arithmetic_(jive_fltop_code op)
+{
+	return (1 << jive_i386_classify_sse);
+}
+
+static jive_regselect_mask
+jive_i386_classify_float_compare_(jive_fltcmp_code op)
+{
+	return (1 << jive_i386_classify_sse);
+}
+
+static jive_regselect_mask
 jive_i386_classify_address_(void)
 {
 	return (1 << jive_i386_classify_gpr);
@@ -68,7 +86,9 @@ const jive_reg_classifier jive_i386_reg_classifier = {
 	.any = (1 << jive_i386_classify_gpr) | (1 << jive_i386_classify_flags),
 	.classify_type = jive_i386_classify_type_,
 	.classify_fixed_arithmetic = jive_i386_classify_fixed_arithmetic_,
+	.classify_float_arithmetic = jive_i386_classify_float_arithmetic_,
 	.classify_fixed_compare = jive_i386_classify_fixed_compare_,
+	.classify_float_compare = jive_i386_classify_float_compare_,
 	.classify_address = jive_i386_classify_address_,
 	
 	.nclasses = 5,
