f963ae923cc889440de492b132df2bb71ef83b46
Nico Reißmann <nico.reissmann@gmail.com>
2013-08-26 16:05:44 +0200

    record: add casting functions for record type
    Implement cast and const_cast functions for record
    type, input, output and gate.
    
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- include/jive/types/record/rcdtype.h
+++ include/jive/types/record/rcdtype.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 2012 Nico Reißmann <nico.reissmann@gmail.com>
+ * Copyright 2011 2012 2013 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
@@ -8,7 +8,7 @@
 
 #include <jive/vsdg/valuetype.h>
 
-struct jive_value_type;
+/* record declaration */
 
 typedef struct jive_record_declaration jive_record_declaration;
 
@@ -17,17 +17,40 @@ struct jive_record_declaration {
 	const jive_value_type ** elements;
 };
 
+/* record type */
+
 typedef struct jive_record_type jive_record_type;
-typedef struct jive_record_input jive_record_input;
-typedef struct jive_record_output jive_record_output;
-typedef struct jive_record_gate jive_record_gate;
 
 extern const jive_type_class JIVE_RECORD_TYPE;
-
 struct jive_record_type {
 	jive_value_type base;
 	const jive_record_declaration * decl;
 };
+
+void
+jive_record_type_init(jive_record_type * self, const jive_record_declaration * decl);
+
+JIVE_EXPORTED_INLINE const jive_record_type *
+jive_record_type_const_cast(const jive_type * self)
+{
+	if (jive_type_isinstance(self, &JIVE_RECORD_TYPE))
+		return (const jive_record_type *)self;
+	else
+		return NULL;
+}
+
+JIVE_EXPORTED_INLINE jive_record_type *
+jive_record_type_cast(jive_type * self)
+{
+	if (jive_type_isinstance(self, &JIVE_RECORD_TYPE))
+		return (jive_record_type *)self;
+	else
+		return NULL;
+}
+
+/* record input */
+
+typedef struct jive_record_input jive_record_input;
 
 extern const jive_input_class JIVE_RECORD_INPUT;
 struct jive_record_input {
@@ -35,11 +58,55 @@ struct jive_record_input {
 	jive_record_type type;
 };
 
+JIVE_EXPORTED_INLINE const jive_record_input *
+jive_record_input_const_cast(const jive_input * self)
+{
+	if (jive_input_isinstance(self, &JIVE_RECORD_INPUT))
+		return (const jive_record_input *)self;
+	else
+		return NULL;
+}
+
+JIVE_EXPORTED_INLINE jive_record_input *
+jive_record_input_cast(jive_input * self)
+{
+	if (jive_input_isinstance(self, &JIVE_RECORD_INPUT))
+		return (jive_record_input *)self;
+	else
+		return NULL;
+}
+
+/* record output */
+
+typedef struct jive_record_output jive_record_output;
+
 extern const jive_output_class JIVE_RECORD_OUTPUT;
 struct jive_record_output {
 	jive_value_output base;
 	jive_record_type type;
 };
+
+JIVE_EXPORTED_INLINE const jive_record_output *
+jive_record_output_const_cast(const jive_output * self)
+{
+	if (jive_output_isinstance(self, &JIVE_RECORD_OUTPUT))
+		return (const jive_record_output *)self;
+	else
+		return 0;
+}
+
+JIVE_EXPORTED_INLINE jive_record_output *
+jive_record_output_cast(jive_output * self)
+{
+	if (jive_output_isinstance(self, &JIVE_RECORD_OUTPUT))
+		return (jive_record_output *)self;
+	else
+		return 0;
+}
+
+/* record gate */
+
+typedef struct jive_record_gate jive_record_gate;
 
 extern const jive_gate_class JIVE_RECORD_GATE;
 struct jive_record_gate {
@@ -47,9 +114,22 @@ struct jive_record_gate {
 	jive_record_type type;
 };
 
-void
-jive_record_type_init(
-	jive_record_type * self,
-	const jive_record_declaration * decl);
+JIVE_EXPORTED_INLINE const jive_record_gate *
+jive_record_gate_const_cast(const jive_gate * self)
+{
+	if (jive_gate_isinstance(self, &JIVE_RECORD_GATE))
+		return (const jive_record_gate *)self;
+	else
+		return NULL;
+}
+
+JIVE_EXPORTED_INLINE jive_record_gate *
+jive_record_gate_cast(jive_gate * self)
+{
+	if (jive_gate_isinstance(self, &JIVE_RECORD_GATE))
+		return (jive_record_gate *)self;
+	else
+		return NULL;
+}
 
 #endif
