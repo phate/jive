3958c75c43497bacd3de92547a513f095cdddd55
Nico Reißmann <nico.reissmann@gmail.com>
2013-08-26 15:25:28 +0200

    address: add casting functions for address type
    Implement cast and const_cast functions for address type, input,
    output and gate.
    
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- include/jive/arch/addresstype.h
+++ include/jive/arch/addresstype.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 2012 Nico Reißmann <nico.reissmann@gmail.com>
+ * Copyright 2011 2012 2013 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
@@ -8,10 +8,9 @@
 
 #include <jive/vsdg/valuetype.h>
 
+/* address type */
+
 typedef struct jive_address_type jive_address_type;
-typedef struct jive_address_input jive_address_input;
-typedef struct jive_address_output jive_address_output;
-typedef struct jive_address_gate jive_address_gate;
 
 extern const jive_type_class JIVE_ADDRESS_TYPE;
 #define JIVE_DECLARE_ADDRESS_TYPE(name) \
@@ -22,11 +21,58 @@ struct jive_address_type {
 	jive_value_type base;
 };
 
+void
+jive_address_type_init(jive_address_type * self);
+
+JIVE_EXPORTED_INLINE const jive_address_type *
+jive_address_type_const_cast(const jive_type * self)
+{
+	if (jive_type_isinstance(self, &JIVE_ADDRESS_TYPE))
+		return (const jive_address_type *)self;
+	else
+		return NULL;
+}
+
+JIVE_EXPORTED_INLINE jive_address_type *
+jive_address_type_cast(jive_type * self)
+{
+	if (jive_type_isinstance(self, &JIVE_ADDRESS_TYPE))
+		return (jive_address_type *)self;
+	else
+		return NULL;
+}
+
+/* address input */
+
+typedef struct jive_address_input jive_address_input;
+
 extern const jive_input_class JIVE_ADDRESS_INPUT;
 struct jive_address_input {
 	jive_value_input base;
 	jive_address_type type;
 };
+
+JIVE_EXPORTED_INLINE const jive_address_input *
+jive_address_input_const_cast(const jive_input * self)
+{
+	if (jive_input_isinstance(self, &JIVE_ADDRESS_INPUT))
+		return (const jive_address_input *)self;
+	else
+		return NULL;
+}
+
+JIVE_EXPORTED_INLINE jive_address_input *
+jive_address_input_cast(jive_input * self)
+{
+	if (jive_input_isinstance(self, &JIVE_ADDRESS_INPUT))
+		return (jive_address_input *)self;
+	else
+		return NULL;
+}
+
+/* address output */
+
+typedef struct jive_address_output jive_address_output;
 
 extern const jive_output_class JIVE_ADDRESS_OUTPUT;
 struct jive_address_output {
@@ -34,13 +80,50 @@ struct jive_address_output {
 	jive_address_type type;
 };
 
+JIVE_EXPORTED_INLINE const jive_address_output *
+jive_address_output_const_cast(const jive_output * self)
+{
+	if (jive_output_isinstance(self, &JIVE_ADDRESS_OUTPUT))
+		return (const jive_address_output *)self;
+	else
+		return NULL;
+}
+
+JIVE_EXPORTED_INLINE jive_address_output *
+jive_address_output_cast(jive_output * self)
+{
+	if (jive_output_isinstance(self, &JIVE_ADDRESS_OUTPUT))
+		return (jive_address_output *)self;
+	else
+		return NULL;
+}
+
+/* address gate */
+
+typedef struct jive_address_gate jive_address_gate;
+
 extern const jive_gate_class JIVE_ADDRESS_GATE;
 struct jive_address_gate {
 	jive_value_gate base;
 	jive_address_type type;
 };
 
-void
-jive_address_type_init(jive_address_type * self);
+JIVE_EXPORTED_INLINE const jive_address_gate *
+jive_address_gate_const_cast(const jive_gate * self)
+{
+	if (jive_gate_isinstance(self, &JIVE_ADDRESS_GATE))
+		return (const jive_address_gate *)self;
+	else
+		return NULL;
+}
+
+JIVE_EXPORTED_INLINE jive_address_gate *
+jive_address_gate_cast(jive_gate * self)
+{
+	if (jive_gate_isinstance(self, &JIVE_ADDRESS_GATE))
+		return (jive_address_gate *)self;
+	else
+		return NULL;
+}
 
 #endif
