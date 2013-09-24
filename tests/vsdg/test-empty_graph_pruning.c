ce6f82886c7eaa7f3968c2dd24826d05b976c043
Nico Reißmann <nico.reissmann@gmail.com>
2013-09-24 04:21:24 +0200

    tests: add test for pruning of an empty graph, i.e. a graph that only contains a graph tail node
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

---
+++ tests/vsdg/test-empty_graph_pruning.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2013 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include "test-registry.h"
+
+#include <locale.h>
+#include <assert.h>
+
+#include <jive/view.h>
+#include <jive/vsdg.h>
+
+static int
+test_main(void)
+{
+	setlocale(LC_ALL, "");
+
+	jive_context * context = jive_context_create();
+	jive_graph * graph = jive_graph_create(context);
+
+	jive_view(graph, stdout);
+
+	jive_graph_prune(graph);
+
+	assert(graph->root_region->nodes.first == graph->root_region->bottom);
+
+	jive_view(graph, stdout);
+
+	jive_graph_destroy(graph);
+	jive_context_assert_clean(context);
+	jive_context_destroy(context);
+
+	return 0;
+}
+
+JIVE_UNIT_TEST_REGISTER("vsdg/test-empty_graph_pruning", test_main);
