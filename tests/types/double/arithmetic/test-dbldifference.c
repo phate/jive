ea6d1a53f4a13986766e905880b5b867fffac7ad
Nico Reißmann <nico.reissmann@gmail.com>
2013-10-24 13:39:58 +0200

    tests: add tests for double-precision floating point operations
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

---
+++ tests/types/double/arithmetic/test-dbldifference.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include "test-registry.h"
+
+#include <locale.h>
+
+#include <jive/types/double/arithmetic/dbldifference.h>
+#include <jive/types/double/dbltype.h>
+#include <jive/view.h>
+#include <jive/vsdg.h>
+#include <jive/vsdg/node-private.h>
+
+static int
+test_main(void)
+{
+	setlocale(LC_ALL, "");
+
+	jive_context * context = jive_context_create();
+	jive_graph * graph = jive_graph_create(context);
+
+	JIVE_DECLARE_DOUBLE_TYPE(dbltype);
+	jive_node * top = jive_node_create(graph->root_region,
+		0, NULL, NULL,
+		2, (const jive_type*[]){dbltype, dbltype});
+
+	jive_output * sub = jive_dbldifference(top->outputs[0], top->outputs[1]);
+
+	jive_node * bottom = jive_node_create(graph->root_region,
+		1, &dbltype, &sub,
+		0, NULL);
+	jive_node_reserve(bottom);
+
+	jive_graph_normalize(graph);
+	jive_graph_prune(graph);
+	jive_view(graph, stdout);
+
+	jive_graph_destroy(graph);
+	jive_context_assert_clean(context);
+	jive_context_destroy(context);
+
+	return 0;
+}
+
+JIVE_UNIT_TEST_REGISTER("types/double/arithmetic/test-dbldifference", test_main)
---
+++ tests/types/double/comparison/test-dblnotequal.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include "test-registry.h"
+
+#include <locale.h>
+
+#include <jive/types/double/comparison/dblnotequal.h>
+#include <jive/types/double/dbltype.h>
+#include <jive/view.h>
+#include <jive/vsdg.h>
+#include <jive/vsdg/node-private.h>
+
+static int
+test_main(void)
+{
+	setlocale(LC_ALL, "");
+
+	jive_context * context = jive_context_create();
+	jive_graph * graph = jive_graph_create(context);
+
+	JIVE_DECLARE_CONTROL_TYPE(ctype);
+	JIVE_DECLARE_DOUBLE_TYPE(dbltype);
+	jive_node * top = jive_node_create(graph->root_region,
+		0, NULL, NULL,
+		2, (const jive_type*[]){dbltype, dbltype});
+
+	jive_output * neq = jive_dblnotequal(top->outputs[0], top->outputs[1]);
+
+	jive_node * bottom = jive_node_create(graph->root_region,
+		1, &ctype, &neq,
+		0, NULL);
+	jive_node_reserve(bottom);
+
+	jive_graph_normalize(graph);
+	jive_graph_prune(graph);
+	jive_view(graph, stdout);
+
+	jive_graph_destroy(graph);
+	assert(jive_context_is_empty(context));
+	jive_context_destroy(context);
+
+	return 0;
+}
+
+JIVE_UNIT_TEST_REGISTER("types/double/comparison/test-dblnotequal", test_main)
