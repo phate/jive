2bf248fd65c6286764a29f8469e1654c19d832fc
Nico Reißmann <nico.reissmann@gmail.com>
2013-10-24 13:55:08 +0200

    double: add all-include header
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

---
+++ include/jive/types/double.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2013 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#ifndef JIVE_TYPES_DOUBLE_H
+#define JIVE_TYPES_DOUBLE_H
+
+#include <jive/types/double/arithmetic.h>
+#include <jive/types/double/comparison.h>
+#include <jive/types/double/dblconstant.h>
+#include <jive/types/double/dbloperation-classes.h>
+#include <jive/types/double/dbltype.h>
+
+#endif
