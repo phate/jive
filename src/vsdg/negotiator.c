1b45f5d808ab1de1187d3e32e252c808c3e285b2
Nico Reißmann <nico.reissmann@gmail.com>
2013-12-09 11:58:24 +0100

    vsdg: add type_check dummy for negotiator split node
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- src/vsdg/negotiator.c
+++ src/vsdg/negotiator.c
@@ -107,6 +107,7 @@ const jive_node_class JIVE_NEGOTIATOR_SPLIT_NODE = {
 	.get_label = jive_node_get_label_, /* inherit */
 	.get_attrs = jive_negotiator_split_node_get_attrs_, /* override */
 	.match_attrs = jive_negotiator_split_node_match_attrs_, /* override */
+	.check_operands = NULL,
 	.create = jive_negotiator_split_node_create_, /* override */
 	.get_aux_rescls = jive_node_get_aux_rescls_ /* inherit */
 };
