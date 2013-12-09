f26425e63717cfa340ac35671446dfe93c316361
Nico Reißmann <nico.reissmann@gmail.com>
2013-12-09 11:59:11 +0100

    vsdg: add type_check dymmy for binary operator
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- src/vsdg/operators/binary.c
+++ src/vsdg/operators/binary.c
@@ -66,6 +66,7 @@ const jive_binary_operation_class JIVE_BINARY_OPERATION_ = {
 		.get_label = jive_node_get_label_, /* inherit */
 		.get_attrs = jive_node_get_attrs_, /* inherit */
 		.match_attrs = jive_node_match_attrs_, /* inherit */
+		.check_operands = NULL,
 		.create = jive_node_create_, /* inherit */
 		.get_aux_rescls = jive_node_get_aux_rescls_ /* inherit */
 	},
