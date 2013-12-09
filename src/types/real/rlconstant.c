a9d5b00de0de94277e13d30e76eeb6bf074c1624
Nico Reißmann <nico.reissmann@gmail.com>
2013-12-09 11:55:17 +0100

    real: add missing name member for node_class of constant operator
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- src/types/real/rlconstant.c
+++ src/types/real/rlconstant.c
@@ -34,6 +34,7 @@ jive_rlconstant_node_create_(struct jive_region * region, const jive_node_attrs 
 
 const jive_node_class JIVE_RLCONSTANT_NODE = {
 	.parent = &JIVE_NULLARY_OPERATION,
+	.name = "RLCONSTANT",
 	.fini = jive_rlconstant_node_fini_, /* override */
 	.get_default_normal_form = jive_nullary_operation_get_default_normal_form_, /* inherit */
 	.get_label = jive_rlconstant_node_get_label_, /* override */
