8f143d8d6bcf4242222d31d026c6d18c6c0e816b
Nico Reißmann <nico.reissmann@gmail.com>
2014-03-20 16:07:00 +0100

    frontend: add all include headers for tacs
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

---
+++ include/jive/frontend/tac/bitstring.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2013 2014 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#ifndef JIVE_FRONTEND_TAC_BITSTRING_H
+#define JIVE_FRONTEND_TAC_BITSTRING_H
+
+#include <jive/frontend/tac/bitstring/arithmetic.h>
+#include <jive/frontend/tac/bitstring/bitconstant.h>
+#include <jive/frontend/tac/bitstring/comparison.h>
+
+#endif
