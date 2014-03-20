25397218041b84f4575c932a3a0860892f5b7a2b
Nico Reißmann <nico.reissmann@gmail.com>
2014-03-20 11:04:25 +0100

    common: define JIVE_ASSERT macro
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- include/jive/common.h
+++ include/jive/common.h
@@ -1,13 +1,17 @@
 /*
  * Copyright 2010 2011 2012 Helge Bahmann <hcb@chaoticmind.net>
+ * Copyright 2013 2014 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
 #ifndef JIVE_COMMON_H
 #define JIVE_COMMON_H
 
+#include <assert.h>
+
+#define JIVE_ASSERT(x) assert(x)
+
 #ifdef JIVE_DEBUG 
-#	include <assert.h>
 #	define JIVE_DEBUG_ASSERT(x) assert(x)
 #else
 #	define JIVE_DEBUG_ASSERT(x) (void)(x)
