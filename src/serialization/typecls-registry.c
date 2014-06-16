3b176d3532da120461b73b154be04cb115be29eb
Helge Bahmann <hcb@chaoticmind.net>
2014-06-16 07:52:01 +0200

    serialization: fix memory leak on program exit
    The type class registry is now an object that gets destroyed on program
    exit. Fix a memory leak exposed as result of that, and swap out home-grown
    hash implementation with std::unordered_map.
    
    Signed-off-by: Nico Reißmann <nico.reissmann@gmail.com>

--- src/serialization/typecls-registry.c
+++ src/serialization/typecls-registry.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 2011 2012 Helge Bahmann <hcb@chaoticmind.net>
+ * Copyright 2010 2011 2012 2013 2014 Helge Bahmann <hcb@chaoticmind.net>
  * See COPYING for terms of redistribution.
  */
 
@@ -8,132 +8,37 @@
 #include <pthread.h>
 
 #include <jive/util/buffer.h>
+#include <jive/util/cstr-map.h>
 #include <jive/util/hash.h>
 #include <jive/util/typeinfo-map.h>
 #include <jive/vsdg/basetype.h>
 
-typedef struct jive_typecls_tag_bucket jive_typecls_tag_bucket;
-struct jive_typecls_tag_bucket {
-	jive_serialization_typecls * first;
-	jive_serialization_typecls * last;
+struct jive_serialization_typecls_registry {
+	~jive_serialization_typecls_registry();
+
+	jive::detail::cstr_map<jive_serialization_typecls *> by_tag_;
+	jive::detail::typeinfo_map<jive_serialization_typecls *> by_cls_;
 };
 
-static jive_typecls_tag_bucket jive_typecls_empty_tag_bucket;
-struct jive_serialization_typecls_registry {
-	inline jive_serialization_typecls_registry()
-		: by_tag(&jive_typecls_empty_tag_bucket)
-		, nbuckets(0)
-		, nitems(0)
-		, mask(0)
-		, initialized(false) {
+jive_serialization_typecls_registry::~jive_serialization_typecls_registry()
+{
+	for (auto element : by_cls_) {
+		jive_serialization_typecls * sercls = element.second;
+		free(sercls->tag);
+		free(sercls);
 	}
-	jive_typecls_tag_bucket * by_tag;
-	size_t nbuckets;
-	size_t nitems;
-	size_t mask;
-	bool initialized;
-	jive::detail::typeinfo_map<const jive_serialization_typecls *> by_cls_;
-};
+}
 
 static std::unique_ptr<jive_serialization_typecls_registry> typecls_registry_singleton;
 static pthread_mutex_t typecls_registry_singleton_lock = PTHREAD_MUTEX_INITIALIZER;
-
-static void
-jive_typecls_tag_chain_remove(jive_typecls_tag_bucket * b, jive_serialization_typecls * sercls)
-{
-	if (sercls->tag_chain.prev)
-		sercls->tag_chain.prev->tag_chain.next = sercls->tag_chain.next;
-	else
-		b->first = sercls->tag_chain.next;
-	if (sercls->tag_chain.next)
-		sercls->tag_chain.next->tag_chain.prev = sercls->tag_chain.prev;
-	else
-		b->last = sercls->tag_chain.prev;
-}
-
-static void
-jive_typecls_tag_chain_insert(jive_typecls_tag_bucket * b, jive_serialization_typecls * sercls)
-{
-	sercls->tag_chain.prev = b->last;
-	sercls->tag_chain.next = 0;
-	if (b->last)
-		b->last->tag_chain.next = sercls;
-	else
-		b->first = sercls;
-	b->last = sercls;
-}
-
-static size_t
-jive_tag_hash(const char * tag)
-{
-	size_t tmp = 0;
-	while (*tag) {
-		tmp = tmp * 5 + * (unsigned char *) tag;
-		++tag;
-	}
-	
-	return tmp;
-}
-
-static bool
-jive_serialization_typecls_registry_rehash(
-	jive_serialization_typecls_registry * self)
-{
-	size_t mask = self->mask;
-	size_t new_nbuckets = self->nbuckets * 2;
-	if (!new_nbuckets)
-		new_nbuckets = 1;
-	size_t new_mask = new_nbuckets - 1;
-	jive_typecls_tag_bucket * new_by_tag = malloc(new_nbuckets * sizeof(*new_by_tag));
-	if (!new_by_tag) {
-		return false;
-	}
-	
-	size_t k;
-	for (k = 0; k < new_nbuckets; ++k)
-		new_by_tag[k].first = new_by_tag[k].last = 0;
-	
-	for (k = 0; k < self->nbuckets; k++) {
-		jive_typecls_tag_bucket * tb = &self->by_tag[k];
-		while (tb->first) {
-			jive_serialization_typecls * tmp = tb->first;
-			jive_typecls_tag_chain_remove(tb, tmp);
-			
-			jive_typecls_tag_bucket * new_tb = new_by_tag + (jive_tag_hash(tmp->tag) & new_mask);
-			jive_typecls_tag_chain_insert(new_tb, tmp);
-		}
-	}
-	
-	if (self->nbuckets) {
-		free(self->by_tag);
-	}
-	self->by_tag = new_by_tag;
-	self->nbuckets = new_nbuckets;
-	self->mask = new_mask;
-	
-	return true;
-}
 
 static bool
 jive_serialization_typecls_registry_insert(
 	jive_serialization_typecls_registry * self,
 	jive_serialization_typecls * sercls)
 {
-	if (self->nbuckets == self->nitems) {
-		if (!jive_serialization_typecls_registry_rehash(self))
-			return false;
-	}
-	
-	size_t mask = self->mask;
-	
-	JIVE_DEBUG_ASSERT(!jive_serialization_typecls_lookup_by_tag(self, sercls->tag));
-	
-	jive_typecls_tag_bucket * tb = self->by_tag + (jive_tag_hash(sercls->tag) & mask);
-	jive_typecls_tag_chain_insert(tb, sercls);
-	
+	self->by_tag_[sercls->tag] = sercls;
 	self->by_cls_[sercls->cls] = sercls;
-	
-	self->nitems ++;
 	
 	return true;
 }
@@ -169,15 +74,12 @@ jive_serialization_typecls_lookup_by_tag(
 	const jive_serialization_typecls_registry * self,
 	const char * tag)
 {
-	jive_typecls_tag_bucket * b = self->by_tag + (jive_tag_hash(tag) & self->mask);
-	const jive_serialization_typecls * sercls = b->first;
-	while (sercls) {
-		if (strcmp(sercls->tag, tag) == 0)
-			return sercls;
-		sercls = sercls->tag_chain.next;
+	auto i = self->by_tag_.find(tag);
+	if (i != self->by_tag_.end()) {
+		return i->second;
+	} else {
+		return nullptr;
 	}
-	
-	return 0;
 }
 
 void
