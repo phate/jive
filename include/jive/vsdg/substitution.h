7f38aec00d2ae5b80c922c062a4a76cf829fcea6
Nico Reißmann <nico.reissmann@gmail.com>
2014-09-26 11:27:36 +0200

    cxx: replace custom hash implementation with std::unordered_map in substitution map
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- include/jive/vsdg/substitution.h
+++ include/jive/vsdg/substitution.h
@@ -1,12 +1,13 @@
 /*
  * Copyright 2010 2011 2012 Helge Bahmann <hcb@chaoticmind.net>
+ * Copyright 2014 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
 #ifndef JIVE_VSDG_SUBSTITUTION_H
 #define JIVE_VSDG_SUBSTITUTION_H
 
-#include <jive/util/hash.h>
+#include <unordered_map>
 
 namespace jive {
 	class gate;
@@ -18,23 +19,11 @@ struct jive_graph;
 struct jive_region;
 
 typedef struct jive_substitution_map jive_substitution_map;
-typedef struct jive_output_substitution jive_output_substitution;
-typedef struct jive_region_substitution jive_region_substitution;
-typedef struct jive_gate_substitution jive_gate_substitution;
-
-typedef struct jive_output_substitution_hash jive_output_substitution_hash;
-typedef struct jive_region_substitution_hash jive_region_substitution_hash;
-typedef struct jive_gate_substitution_hash jive_gate_substitution_hash;
-JIVE_DECLARE_HASH_TYPE(jive_output_substitution_hash, jive_output_substitution, const jive::output *,
-	original, hash_chain);
-JIVE_DECLARE_HASH_TYPE(jive_region_substitution_hash, jive_region_substitution, const struct jive_region *, original, hash_chain);
-JIVE_DECLARE_HASH_TYPE(jive_gate_substitution_hash, jive_gate_substitution, const jive::gate *,
-	original, hash_chain);
 
 struct jive_substitution_map {
-	jive_output_substitution_hash output_hash;
-	jive_region_substitution_hash region_hash;
-	jive_gate_substitution_hash gate_hash;
+	std::unordered_map<const jive::output*, jive::output*> output_hash;
+	std::unordered_map<const jive_region*, jive_region*> region_hash;
+	std::unordered_map<const jive::gate*, jive::gate*> gate_hash;
 };
 
 jive_substitution_map *
@@ -49,10 +38,12 @@ jive_substitution_map_lookup_output(const jive_substitution_map * self,
 	const jive::output * original);
 
 void
-jive_substitution_map_add_region(jive_substitution_map * self, const struct jive_region * original, struct jive_region * substitute);
+jive_substitution_map_add_region(jive_substitution_map * self, const struct jive_region * original,
+	struct jive_region * substitute);
 
 struct jive_region *
-jive_substitution_map_lookup_region(const jive_substitution_map * self, const struct jive_region * original);
+jive_substitution_map_lookup_region(const jive_substitution_map * self,
+	const struct jive_region * original);
 
 void
 jive_substitution_map_add_gate(jive_substitution_map * self, const jive::gate * original,
--- src/vsdg/substitution.c
+++ src/vsdg/substitution.c
@@ -1,167 +1,72 @@
 /*
  * Copyright 2010 2011 2012 Helge Bahmann <hcb@chaoticmind.net>
+ * Copyright 2014 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
-#include <jive/context.h>
-
 #include <jive/vsdg/substitution.h>
-
-struct jive_output_substitution {
-	const jive::output * original;
-	jive::output * substitute;
-	
-	struct {
-		jive_output_substitution * prev;
-		jive_output_substitution * next;
-	} hash_chain;
-};
-
-struct jive_region_substitution {
-	const struct jive_region * original;
-	struct jive_region * substitute;
-	
-	struct {
-		jive_region_substitution * prev;
-		jive_region_substitution * next;
-	} hash_chain;
-};
-
-struct jive_gate_substitution {
-	const jive::gate * original;
-	jive::gate * substitute;
-	
-	struct {
-		jive_gate_substitution * prev;
-		jive_gate_substitution * next;
-	} hash_chain;
-};
-
-JIVE_DEFINE_HASH_TYPE(jive_output_substitution_hash, jive_output_substitution,
-	const jive::output *, original, hash_chain);
-JIVE_DEFINE_HASH_TYPE(jive_region_substitution_hash, jive_region_substitution, const struct jive_region *, original, hash_chain);
-JIVE_DEFINE_HASH_TYPE(jive_gate_substitution_hash, jive_gate_substitution, const jive::gate *,
-	original, hash_chain);
 
 jive_substitution_map *
 jive_substitution_map_create(struct jive_context * context)
 {
-	jive_substitution_map * map = new jive_substitution_map;
-	jive_output_substitution_hash_init(&map->output_hash, context);
-	jive_region_substitution_hash_init(&map->region_hash, context);
-	jive_gate_substitution_hash_init(&map->gate_hash, context);
-	return map;
+	return new jive_substitution_map;
 }
 
 void
 jive_substitution_map_add_output(jive_substitution_map * self, const jive::output * original,
 	jive::output * substitute)
 {
-	jive_output_substitution * subst = jive_output_substitution_hash_lookup(&self->output_hash, original);
-	if (!subst) {
-		subst = jive_context_malloc(self->output_hash.context, sizeof(*subst));
-		subst->original = original;
-		subst->substitute = substitute;
-		jive_output_substitution_hash_insert(&self->output_hash, subst);
-	} else {
-		subst->substitute = substitute;
-	}
+	self->output_hash[original] = substitute;
 }
 
 jive::output *
 jive_substitution_map_lookup_output(const jive_substitution_map * self,
 	const jive::output * original)
 {
-	jive_output_substitution * subst = jive_output_substitution_hash_lookup(&self->output_hash, original);
-	if (!subst)
-		return NULL;
+	auto i = self->output_hash.find(original);
+	if (i == self->output_hash.end())
+		return nullptr;
 	else
-		return subst->substitute;
+		return i->second;
 }
 
 void
-jive_substitution_map_add_region(jive_substitution_map * self, const struct jive_region * original, struct jive_region * substitute)
+jive_substitution_map_add_region(jive_substitution_map * self, const struct jive_region * original,
+	struct jive_region * substitute)
 {
-	jive_region_substitution * subst = jive_region_substitution_hash_lookup(&self->region_hash, original);
-	if (!subst) {
-		subst = jive_context_malloc(self->region_hash.context, sizeof(*subst));
-		subst->original = original;
-		subst->substitute = substitute;
-		jive_region_substitution_hash_insert(&self->region_hash, subst);
-	} else {
-		subst->substitute = substitute;
-	}
+	self->region_hash[original] = substitute;
 }
 
 struct jive_region *
-jive_substitution_map_lookup_region(const jive_substitution_map * self, const struct jive_region * original)
+jive_substitution_map_lookup_region(const jive_substitution_map * self,
+	const struct jive_region * original)
 {
-	jive_region_substitution * subst = jive_region_substitution_hash_lookup(&self->region_hash, original);
-	if (!subst)
-		return NULL;
+	auto i = self->region_hash.find(original);
+	if (i == self->region_hash.end())
+		return nullptr;
 	else
-		return subst->substitute;
+		return i->second;
 }
 
 void
 jive_substitution_map_add_gate(jive_substitution_map * self, const jive::gate * original,
 	jive::gate * substitute)
 {
-	jive_gate_substitution * subst = jive_gate_substitution_hash_lookup(&self->gate_hash, original);
-	if (!subst) {
-		subst = jive_context_malloc(self->gate_hash.context, sizeof(*subst));
-		subst->original = original;
-		subst->substitute = substitute;
-		jive_gate_substitution_hash_insert(&self->gate_hash, subst);
-	} else {
-		subst->substitute = substitute;
-	}
+	self->gate_hash[original] = substitute;
 }
 
 jive::gate *
 jive_substitution_map_lookup_gate(const jive_substitution_map * self, const jive::gate * original)
 {
-	jive_gate_substitution * subst = jive_gate_substitution_hash_lookup(&self->gate_hash, original);
-	if (!subst)
-		return NULL;
+	auto i = self->gate_hash.find(original);
+	if (i == self->gate_hash.end())
+		return nullptr;
 	else
-		return subst->substitute;
+		return i->second;
 }
 
 void
 jive_substitution_map_destroy(jive_substitution_map * self)
 {
-	jive_context * context = self->output_hash.context;
-	
-	struct jive_output_substitution_hash_iterator i;
-	i = jive_output_substitution_hash_begin(&self->output_hash);
-	while(i.entry) {
-		jive_output_substitution * subst = i.entry;
-		jive_output_substitution_hash_iterator_next(&i);
-		jive_output_substitution_hash_remove(&self->output_hash, subst);
-		jive_context_free(context, subst);
-	}
-	
-	struct jive_region_substitution_hash_iterator j;
-	j = jive_region_substitution_hash_begin(&self->region_hash);
-	while(j.entry) {
-		jive_region_substitution * subst = j.entry;
-		jive_region_substitution_hash_iterator_next(&j);
-		jive_region_substitution_hash_remove(&self->region_hash, subst);
-		jive_context_free(context, subst);
-	}
-	
-	struct jive_gate_substitution_hash_iterator k;
-	k = jive_gate_substitution_hash_begin(&self->gate_hash);
-	while(k.entry) {
-		jive_gate_substitution * subst = k.entry;
-		jive_gate_substitution_hash_iterator_next(&k);
-		jive_gate_substitution_hash_remove(&self->gate_hash, subst);
-		jive_context_free(context, subst);
-	}
-	
-	jive_output_substitution_hash_fini(&self->output_hash);
-	jive_region_substitution_hash_fini(&self->region_hash);
-	jive_gate_substitution_hash_fini(&self->gate_hash);
 	delete self;
 }
