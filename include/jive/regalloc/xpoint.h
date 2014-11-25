ad91ff0c0ebebdbb69a560b9b6d45bccc81362a9
Nico Reißmann <nico.reissmann@gmail.com>
2014-11-25 13:12:34 +0100

    lint: fix copyright headers in regalloc subfolder
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- include/jive/regalloc/xpoint.h
+++ include/jive/regalloc/xpoint.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2010 2011 2012 2014 Helge Bahmann <hcb@chaoticmind.net>
+ * Copyright 2014 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
