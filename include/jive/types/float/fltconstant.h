da5b97c1f0f05ca04466c485db27a9c2d9960697
Nico Reißmann <nico.reissmann@gmail.com>
2014-11-26 22:01:35 +0100

    lint: fix copyright headers in float subfolder
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- include/jive/types/float/fltconstant.h
+++ include/jive/types/float/fltconstant.h
@@ -1,6 +1,6 @@
 /*
  * Copyright 2014 Helge Bahmann <hcb@chaoticmind.net>
- * Copyright 2012 2013 Nico Reißmann <nico.reissmann@gmail.com>
+ * Copyright 2012 2013 2014 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
