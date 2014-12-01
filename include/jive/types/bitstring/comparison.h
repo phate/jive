02ae8fcfbf8a2d12095d9ed48463788bf0fe7e25
Nico Reißmann <nico.reissmann@gmail.com>
2014-12-01 11:42:05 +0100

    lint: fix lexicographical ordering of includes in bitstring subfolder
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- include/jive/types/bitstring/comparison.h
+++ include/jive/types/bitstring/comparison.h
@@ -8,13 +8,13 @@
 
 #include <jive/types/bitstring/comparison/bitequal.h>
 #include <jive/types/bitstring/comparison/bitnotequal.h>
-#include <jive/types/bitstring/comparison/bitsless.h>
-#include <jive/types/bitstring/comparison/bituless.h>
-#include <jive/types/bitstring/comparison/bitslesseq.h>
-#include <jive/types/bitstring/comparison/bitulesseq.h>
 #include <jive/types/bitstring/comparison/bitsgreater.h>
-#include <jive/types/bitstring/comparison/bitugreater.h>
 #include <jive/types/bitstring/comparison/bitsgreatereq.h>
+#include <jive/types/bitstring/comparison/bitsless.h>
+#include <jive/types/bitstring/comparison/bitslesseq.h>
+#include <jive/types/bitstring/comparison/bitugreater.h>
 #include <jive/types/bitstring/comparison/bitugreatereq.h>
+#include <jive/types/bitstring/comparison/bituless.h>
+#include <jive/types/bitstring/comparison/bitulesseq.h>
 
 #endif
