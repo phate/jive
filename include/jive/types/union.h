3c93d61c6fd9f67d8071f12747c5f114efe38d36
Nico Reißmann <nico.reissmann@gmail.com>
2014-12-01 11:51:13 +0100

    lint: fix lexicographical ordering of includes in union.h
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- include/jive/types/union.h
+++ include/jive/types/union.h
@@ -6,8 +6,8 @@
 #ifndef JIVE_TYPES_UNION_H
 #define JIVE_TYPES_UNION_H
 
+#include <jive/types/union/unnchoose.h>
 #include <jive/types/union/unntype.h>
 #include <jive/types/union/unnunify.h>
-#include <jive/types/union/unnchoose.h>
 
 #endif
