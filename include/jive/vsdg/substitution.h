bc12809bc25d3b02038e7d5ff774f7810e58ecc7
Nico Reißmann <nico.reissmann@gmail.com>
2015-03-22 16:42:18 +0100

    cxx: add new substitution map
    Signed-off-by: Helge Bahmann <hcb@chaoticmind.net>

--- include/jive/vsdg/substitution.h
+++ include/jive/vsdg/substitution.h
@@ -1,11 +1,13 @@
 /*
  * Copyright 2010 2011 2012 Helge Bahmann <hcb@chaoticmind.net>
- * Copyright 2014 Nico Reißmann <nico.reissmann@gmail.com>
+ * Copyright 2014 2015 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
 #ifndef JIVE_VSDG_SUBSTITUTION_H
 #define JIVE_VSDG_SUBSTITUTION_H
+
+#include <jive/common.h>
 
 #include <unordered_map>
 
@@ -17,9 +19,60 @@ namespace jive {
 struct jive_graph;
 struct jive_region;
 
+namespace jive {
+
+class substitution_map final {
+public:
+	inline jive::output *
+	lookup(const jive::output * original) const noexcept
+	{
+		auto i = output_map_.find(original);
+		return i != output_map_.end() ? i->second : nullptr;
+	}
+
+	inline jive_region *
+	lookup(const jive_region * original) const noexcept
+	{
+		auto i = region_map_.find(original);
+		return i != region_map_.end() ? i->second : nullptr;
+	}
+
+	inline jive::gate *
+	lookup(const jive::gate * original) const noexcept
+	{
+		auto i = gate_map_.find(original);
+		return i != gate_map_.end() ? i->second : nullptr;
+	}
+
+	inline void
+	insert(const jive::output * original, jive::output * substitute)
+	{
+		output_map_[original] = substitute;
+	}
+
+	inline void
+	insert(const jive_region * original, jive_region * substitute)
+	{
+		region_map_[original] = substitute;
+	}
+	inline void
+	insert(const jive::gate * original, jive::gate * substitute)
+	{
+		gate_map_[original] = substitute;
+	}
+
+private:
+	std::unordered_map<const jive::gate*, jive::gate*> gate_map_;
+	std::unordered_map<const jive_region*, jive_region*> region_map_;
+	std::unordered_map<const jive::output*, jive::output*> output_map_;
+};
+
+}
+
 typedef struct jive_substitution_map jive_substitution_map;
 
 struct jive_substitution_map {
+	jive::substitution_map map;
 	std::unordered_map<const jive::output*, jive::output*> output_hash;
 	std::unordered_map<const jive_region*, jive_region*> region_hash;
 	std::unordered_map<const jive::gate*, jive::gate*> gate_hash;
--- src/vsdg/substitution.c
+++ src/vsdg/substitution.c
@@ -1,6 +1,6 @@
 /*
  * Copyright 2010 2011 2012 Helge Bahmann <hcb@chaoticmind.net>
- * Copyright 2014 Nico Reißmann <nico.reissmann@gmail.com>
+ * Copyright 2014 2015 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
@@ -16,53 +16,41 @@ void
 jive_substitution_map_add_output(jive_substitution_map * self, const jive::output * original,
 	jive::output * substitute)
 {
-	self->output_hash[original] = substitute;
+	self->map.insert(original, substitute);
 }
 
 jive::output *
 jive_substitution_map_lookup_output(const jive_substitution_map * self,
 	const jive::output * original)
 {
-	auto i = self->output_hash.find(original);
-	if (i == self->output_hash.end())
-		return nullptr;
-	else
-		return i->second;
+	return self->map.lookup(original);
 }
 
 void
 jive_substitution_map_add_region(jive_substitution_map * self, const struct jive_region * original,
 	struct jive_region * substitute)
 {
-	self->region_hash[original] = substitute;
+	self->map.insert(original, substitute);
 }
 
 struct jive_region *
 jive_substitution_map_lookup_region(const jive_substitution_map * self,
 	const struct jive_region * original)
 {
-	auto i = self->region_hash.find(original);
-	if (i == self->region_hash.end())
-		return nullptr;
-	else
-		return i->second;
+	return self->map.lookup(original);
 }
 
 void
 jive_substitution_map_add_gate(jive_substitution_map * self, const jive::gate * original,
 	jive::gate * substitute)
 {
-	self->gate_hash[original] = substitute;
+	self->map.insert(original, substitute);
 }
 
 jive::gate *
 jive_substitution_map_lookup_gate(const jive_substitution_map * self, const jive::gate * original)
 {
-	auto i = self->gate_hash.find(original);
-	if (i == self->gate_hash.end())
-		return nullptr;
-	else
-		return i->second;
+	return self->map.lookup(original);
 }
 
 void
