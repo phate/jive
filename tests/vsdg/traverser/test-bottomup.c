b8d3427d89570b61211be215d9c97b308c6802c4
Nico Reißmann <nico.reissmann@gmail.com>
2017-02-27 13:59:01 +0100

    tests: move all bottomup traverser tests into test-bottomup.c

--- tests/vsdg/traverser/test-bottomup.c
+++ tests/vsdg/traverser/test-bottomup.c
@@ -31,10 +31,68 @@ test_initialization()
 	assert(n2_visited);
 }
 
+static void
+test_basic_traversal()
+{
+	jive_graph graph;
+	jive_test_value_type type;
+	auto n1 = jive_test_node_create(graph.root(), {}, {}, {&type, &type});
+	auto n2 = jive_test_node_create(graph.root(), {&type, &type}, {n1->output(0), n1->output(1)},
+		{&type});
+
+	graph.root()->bottom()->add_input(&type, n2->output(0));
+
+	{
+		jive::node * tmp;
+		jive::bottomup_traverser trav(graph.root());
+		tmp = trav.next();
+		assert(tmp == graph.root()->bottom());
+		tmp = trav.next();
+		assert(tmp == n2);
+		tmp = trav.next();
+		assert(tmp == n1);
+		tmp = trav.next();
+		assert(tmp == 0);
+	}
+
+	assert(!graph.has_active_traversers());
+}
+
+static void
+test_order_enforcement_traversal()
+{
+	jive_graph graph;
+	jive_test_value_type type;
+	auto n1 = jive_test_node_create(graph.root(), {}, {}, {&type, &type});
+	auto n2 = jive_test_node_create(graph.root(), {&type}, {n1->output(0)}, {&type});
+	auto n3 = jive_test_node_create(graph.root(), {&type, &type}, {n2->output(0), n1->output(1)},
+		{&type});
+
+	jive::node * tmp;
+	{
+		jive::bottomup_traverser trav(graph.root());
+
+		tmp = trav.next();
+		assert(tmp == n3);
+		tmp = trav.next();
+		assert(tmp == n2);
+		tmp = trav.next();
+		assert(tmp == graph.root()->bottom());
+		tmp = trav.next();
+		assert(tmp == n1);
+		tmp = trav.next();
+		assert(tmp == nullptr);
+	}
+
+	assert(!graph.has_active_traversers());
+}
+
 static int
 test_main()
 {
 	test_initialization();
+	test_basic_traversal();
+	test_order_enforcement_traversal();
 
 	return 0;
 }
