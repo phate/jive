920483c55502969d239137959a0505be3558cfc5
Nico Reißmann <nico.reissmann@gmail.com>
2017-03-01 11:03:01 +0100

    vsdg: add structural_normal_form class

---
+++ src/vsdg/operators/structural-normal-form.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2017 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include <jive/vsdg/operators/structural-normal-form.h>
+
+namespace jive {
+
+structural_normal_form::~structural_normal_form() noexcept
+{}
+
+structural_normal_form::structural_normal_form(
+	const std::type_info & operator_class,
+	jive::node_normal_form * parent,
+	jive::graph * graph) noexcept
+	: node_normal_form(operator_class, parent, graph)
+{}
+
+}
