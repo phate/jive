7d120a96171100148abee75dac0074ce80618515
Nico Reißmann <nico.reissmann@gmail.com>
2017-04-04 09:16:28 +0200

    vsdg: add new builder interface for phis

--- include/jive/vsdg/phi.h
+++ include/jive/vsdg/phi.h
@@ -53,6 +53,106 @@ jive_phi_region_const_cast(const jive::region * region)
 	return nullptr;
 }
 
+namespace jive {
+
+class phi_builder;
+
+class recvar final {
+	friend phi_builder;
+
+private:
+	inline constexpr
+	recvar(jive::oport * value)
+	: value_(value)
+	{}
+
+public:
+	inline void
+	set_value(jive::oport * value)
+	{
+		value_ = value;
+	}
+
+	inline jive::oport *
+	value() const noexcept
+	{
+		return value_;
+	}
+
+private:
+	jive::oport * value_;
+};
+
+class phi_builder final {
+public:
+	inline
+	phi_builder() noexcept
+	: node_(nullptr)
+	{}
+
+	inline jive::region *
+	begin(jive::region * parent)
+	{
+		if (node_)
+			node_->subregion(0);
+
+		node_ = parent->add_structural_node(jive::phi_op(), 1);
+		return node_->subregion(0);
+	}
+
+	inline jive::region *
+	region() const noexcept
+	{
+		return node_ ? node_->subregion(0) : nullptr;
+	}
+
+	inline jive::oport *
+	add_dependency(jive::oport * value)
+	{
+		if (!node_)
+			return nullptr;
+
+		auto input = node_->add_input(&value->type(), value);
+		return node_->subregion(0)->add_argument(input, value->type());
+	}
+
+	inline std::shared_ptr<jive::recvar>
+	add_recvar(const jive::base::type & type)
+	{
+		if (!node_)
+			return nullptr;
+
+		auto argument = node_->subregion(0)->add_argument(nullptr, type);
+		recvars_.push_back(std::shared_ptr<recvar>(new recvar(argument)));
+		return recvars_[recvars_.size()-1];
+	}
+
+	inline jive::structural_node *
+	end()
+	{
+		if (!node_)
+			return nullptr;
+
+		for (const auto & rv : recvars_) {
+			auto output = node_->add_output(&rv->value()->type());
+			node_->subregion(0)->add_result(rv->value(), output, rv->value()->type());
+			rv->set_value(output);
+		}
+
+		auto node = node_;
+		node_ = nullptr;
+		recvars_.clear();
+
+		return node;
+	}
+
+private:
+	jive::structural_node * node_;
+	std::vector<std::shared_ptr<recvar>> recvars_;
+};
+
+}
+
 typedef struct jive_phi jive_phi;
 typedef struct jive_phi_fixvar jive_phi_fixvar;
 
