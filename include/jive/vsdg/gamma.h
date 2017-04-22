85daf921be3cb8fb9bc0fd9a2c72ec2e2229e11e
Nico Reißmann <nico.reissmann@gmail.com>
2017-04-22 12:31:26 +0200

    vsdg: add convenience interface for gamma node

--- include/jive/vsdg/gamma.h
+++ include/jive/vsdg/gamma.h
@@ -47,74 +47,105 @@ private:
 	jive::ctl::type predicate_type_;
 };
 
+class gamma;
 class gamma_builder;
 
 class entryvar final {
+	friend gamma;
 	friend gamma_builder;
 
 private:
-	inline
-	entryvar(const std::vector<jive::argument*> & arguments)
-	: arguments_(arguments)
+	inline constexpr
+	entryvar(jive::structural_input * input)
+	: input_(input)
 	{}
 
 public:
+	inline bool
+	operator==(const entryvar & other) const noexcept
+	{
+		return input_ == other.input_;
+	}
+
+	inline bool
+	operator!=(const entryvar & other) const noexcept
+	{
+		return !(*this == other);
+	}
+
 	inline jive::structural_input *
 	input() const noexcept
 	{
-		JIVE_DEBUG_ASSERT(arguments_.size() != 0);
-		return arguments_[0]->input();
+		return input_;
 	}
 
 	inline size_t
 	narguments() const noexcept
 	{
-		return arguments_.size();
+		return static_cast<const jive::structural_node*>(input()->node())->nsubregions();
 	}
 
 	inline jive::argument *
 	argument(size_t n) const noexcept
 	{
 		JIVE_DEBUG_ASSERT(n < narguments());
-		return arguments_[n];
+		auto subregion = static_cast<const jive::structural_node*>(input()->node())->subregion(n);
+		auto argument = subregion->argument(input()->index()-1);
+		JIVE_DEBUG_ASSERT(argument->input() == input());
+		return argument;
 	}
 
 private:
-	std::vector<jive::argument*> arguments_;
+	jive::structural_input * input_;
 };
 
 class exitvar final {
+	friend gamma;
 	friend gamma_builder;
 
 private:
-	inline
-	exitvar(const std::vector<jive::result*> & results)
-	: results_(results)
+	inline constexpr
+	exitvar(jive::structural_output * output)
+	: output_(output)
 	{}
 
 public:
+	inline bool
+	operator==(const exitvar & other) const noexcept
+	{
+		return output_ == other.output_;
+	}
+
+	inline bool
+	operator!=(const exitvar & other) const noexcept
+	{
+		return !(*this == other);
+	}
+
 	inline jive::structural_output *
 	output() const noexcept
 	{
-		JIVE_DEBUG_ASSERT(results_.size() != 0);
-		return results_[0]->output();
+		return output_;
 	}
 
 	inline size_t
 	nresults() const noexcept
 	{
-		return results_.size();
+		return static_cast<const jive::structural_node*>(output()->node())->nsubregions();
 	}
 
 	inline jive::result *
 	result(size_t n) const noexcept
 	{
 		JIVE_DEBUG_ASSERT(n < nresults());
-		return results_[n];
+		auto subregion = static_cast<const jive::structural_node*>(output()->node())->subregion(n);
+		auto result = subregion->result(output()->index());
+		JIVE_DEBUG_ASSERT(result->output() == output());
+		return result;
 	}
 
 private:
-	std::vector<jive::result*> results_;
+	jive::structural_output * output_;
 };
 
 class gamma_builder final {
@@ -164,12 +195,11 @@ public:
 		if (!node_)
 			return nullptr;
 
-		std::vector<jive::argument*> arguments;
 		auto input = node_->add_input(&origin->type(), origin);
 		for (size_t n = 0; n < nsubregions(); n++)
-			arguments.push_back(node_->subregion(n)->add_argument(input, origin->type()));
+			node_->subregion(n)->add_argument(input, origin->type());
 
-		return std::shared_ptr<entryvar>(new entryvar(arguments));
+		return std::shared_ptr<entryvar>(new entryvar(input));
 	}
 
 	inline std::shared_ptr<jive::exitvar>
@@ -183,12 +213,11 @@ public:
 		JIVE_DEBUG_ASSERT(values.size() != 0);
 
 		auto type = &values[0]->type();
-		std::vector<jive::result*> results;
 		auto output = node_->add_output(type);
 		for (size_t n = 0; n < nsubregions(); n++)
-			results.push_back(node_->subregion(n)->add_result(values[n], output, *type));
+			node_->subregion(n)->add_result(values[n], output, *type);
 
-		return std::shared_ptr<exitvar>(new exitvar(results));
+		return std::shared_ptr<exitvar>(new exitvar(output));
 	}
 
 	inline jive::structural_node *
@@ -197,6 +226,181 @@ public:
 		auto node = node_;
 		node_ = nullptr;
 		return node;
+	}
+
+private:
+	jive::structural_node * node_;
+};
+
+class gamma final {
+public:
+	inline
+	gamma(jive::structural_node * node)
+	: node_(node)
+	{
+		if (!dynamic_cast<const jive::gamma_op*>(&node->operation()))
+			throw jive::compiler_error("Expected gamma node.");
+	}
+
+private:
+	class entryvar_iterator {
+	public:
+		inline constexpr
+		entryvar_iterator(jive::structural_input * input) noexcept
+		: var_(input)
+		{}
+
+		inline const entryvar_iterator &
+		operator++() noexcept
+		{
+			auto input = var_.input();
+			if (input == nullptr)
+				return *this;
+
+			auto node = input->node();
+			auto index = input->index();
+			if (index == node->ninputs()-1) {
+				var_ = entryvar(nullptr);
+				return *this;
+			}
+
+			var_ = entryvar(static_cast<structural_input*>(node->input(++index)));
+			return *this;
+		}
+
+		inline const entryvar_iterator
+		operator++(int) noexcept
+		{
+			entryvar_iterator it(*this);
+			++(*this);
+			return it;
+		}
+
+		inline bool
+		operator==(const entryvar_iterator & other) const noexcept
+		{
+			return var_ == other.var_;
+		}
+
+		inline bool
+		operator!=(const entryvar_iterator & other) const noexcept
+		{
+			return !(*this == other);
+		}
+
+		inline entryvar &
+		operator*() noexcept
+		{
+			return var_;
+		}
+
+		inline entryvar *
+		operator->() noexcept
+		{
+			return &var_;
+		}
+
+	private:
+		entryvar var_;
+	};
+
+	class exitvar_iterator {
+	public:
+		inline constexpr
+		exitvar_iterator(jive::structural_output * output) noexcept
+		: var_(output)
+		{}
+
+		inline const exitvar_iterator &
+		operator++() noexcept
+		{
+			auto output = var_.output();
+			if (output == nullptr)
+				return *this;
+
+			auto node = output->node();
+			auto index = output->index();
+			if (index == node->noutputs()-1) {
+				var_ = exitvar(nullptr);
+				return *this;
+			}
+
+			var_ = exitvar(static_cast<structural_output*>(node->output(++index)));
+			return *this;
+		}
+
+		inline const exitvar_iterator
+		operator++(int) noexcept
+		{
+			exitvar_iterator it(*this);
+			++(*this);
+			return it;
+		}
+
+		inline bool
+		operator==(const exitvar_iterator & other) const noexcept
+		{
+			return var_ == other.var_;
+		}
+
+		inline bool
+		operator!=(const exitvar_iterator & other) const noexcept
+		{
+			return !(*this == other);
+		}
+
+		inline exitvar &
+		operator*() noexcept
+		{
+			return var_;
+		}
+
+		inline exitvar *
+		operator->() noexcept
+		{
+			return &var_;
+		}
+
+	private:
+		exitvar var_;
+	};
+
+public:
+	inline jive::structural_node *
+	node() const noexcept
+	{
+		return node_;
+	}
+
+	inline jive::structural_input *
+	predicate() const noexcept
+	{
+		JIVE_DEBUG_ASSERT(dynamic_cast<const jive::ctl::type*>(&node()->input(0)->type()));
+		return node()->input(0);
+	}
+
+	inline gamma::entryvar_iterator
+	begin_entryvar()
+	{
+		return entryvar_iterator(node()->input(1));
+	}
+
+	inline gamma::entryvar_iterator
+	end_entryvar()
+	{
+		return entryvar_iterator(nullptr);
+	}
+
+	inline gamma::exitvar_iterator
+	begin_exitvar()
+	{
+		return exitvar_iterator(node()->output(0));
+	}
+
+	inline gamma::exitvar_iterator
+	end_exitvar()
+	{
+		return exitvar_iterator(nullptr);
 	}
 
 private:
