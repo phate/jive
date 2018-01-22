e6a75533edcfa2828b5b367b38ba8eb2ab27779f
Nico Reißmann <nico.reissmann@gmail.com>
2018-01-22 10:54:10 +0100

    rvsdg: add is_structural_node function

--- include/jive/rvsdg/structural-node.h
+++ include/jive/rvsdg/structural-node.h
@@ -74,6 +74,12 @@ private:
 	std::vector<std::unique_ptr<jive::region>> subregions_;
 };
 
+static inline bool
+is_structural_node(const jive::node * node) noexcept
+{
+	return dynamic_cast<const jive::structural_node*>(node) != nullptr;
+}
+
 /* structural input class */
 
 typedef jive::detail::intrusive_list<
