3d19a64d81a9d37f58dd390bd5e403ffd3fd2019
Nico Reißmann <nico.reissmann@gmail.com>
2018-02-13 22:21:09 +0100

    tests: split up test-address into tests for individual operators

--- tests/arch/test-address.c
+++ tests/arch/test-address.c
@@ -17,71 +17,146 @@
 
 #include "testnodes.h"
 
-static int test_main(void)
+static int
+test_memberof()
+{
+	using namespace jive;
+
+	jive::graph graph;
+	auto dcl = rcddeclaration::create(&graph, {&bit32, &bit32});
+
+	auto i0 = graph.add_import(addrtype::instance(), "");
+
+	auto m1 = memberof_op::create(i0, dcl, 0);
+	auto m2 = memberof_op::create(i0, dcl, 0);
+	auto m3 = memberof_op::create(i0, dcl, 1);
+
+	auto c1 = containerof_op::create(m1, dcl, 0);
+
+	auto ex0 = graph.add_export(c1, "");
+	auto ex1 = graph.add_export(m2, "");
+	auto ex2 = graph.add_export(m3, "");
+
+	view(graph, stdout);
+
+	assert(ex0->origin() == i0);
+	assert(is_memberof_node(ex1->origin()->node()));
+	assert(is_memberof_node(ex2->origin()->node()));
+
+	memlayout_mapper_simple mapper(4);
+	transform_address(ex1->origin()->node(), mapper);
+	transform_address(ex2->origin()->node(), mapper);
+
+	graph.prune();
+	view(graph, stdout);
+
+	assert(is_bit2addr_node(ex1->origin()->node()));
+	assert(is_bit2addr_node(ex2->origin()->node()));
+
+	return 0;
+}
+
+JIVE_UNIT_TEST_REGISTER("arch/test-memberof", test_memberof)
+
+static int
+test_containerof()
+{
+	using namespace jive;
+
+	jive::graph graph;
+	auto dcl = rcddeclaration::create(&graph, {&bit32, &bit32});
+
+	auto i0 = graph.add_import(addrtype::instance(), "");
+
+	auto c1 = containerof_op::create(i0, dcl, 0);
+	auto c2 = containerof_op::create(i0, dcl, 0);
+	auto c3 = containerof_op::create(i0, dcl, 1);
+
+	auto m1 = memberof_op::create(c1, dcl, 0);
+
+	auto ex0 = graph.add_export(m1, "");
+	auto ex1 = graph.add_export(c2, "");
+	auto ex2 = graph.add_export(c3, "");
+
+	view(graph, stdout);
+
+	assert(ex0->origin() == i0);
+	assert(is_containerof_node(ex1->origin()->node()));
+	assert(is_containerof_node(ex2->origin()->node()));
+
+	memlayout_mapper_simple mapper(4);
+	transform_address(ex1->origin()->node(), mapper);
+	transform_address(ex2->origin()->node(), mapper);
+
+	graph.prune();
+	view(graph, stdout);
+
+	assert(is_bit2addr_node(ex1->origin()->node()));
+	assert(is_bit2addr_node(ex2->origin()->node()));
+
+	return 0;
+}
+
+JIVE_UNIT_TEST_REGISTER("arch/test-containerof", test_containerof)
+
+static int
+test_arraysubscript()
 {
 	using namespace jive;
 
 	jive::graph graph;
 
-	addrtype at;
-	auto dcl = rcddeclaration::create(&graph, {&bit32, &bit32});
+	auto i0 = graph.add_import(addrtype::instance(), "");
+	auto i1 = graph.add_import(bit32, "");
 
-	auto i0 = graph.add_import(at, "");
-	auto i1 = graph.add_import(at, "");
+	auto as1 = arraysubscript_op::create(i0, bit32, i1);
 
-	auto memb1 = memberof_op::create(i0, dcl, 0);
-	auto memb2 = memberof_op::create(i0, dcl, 1);
-	
-	auto cont1 = containerof_op::create(memb1, dcl, 0);
-	auto cont2 = containerof_op::create(memb2, dcl, 0);
-	
-	auto cont3 = containerof_op::create(i1, dcl, 0);
-	
-	auto memb3 = memberof_op::create(cont3, dcl, 0);
-	auto memb4 = memberof_op::create(cont3, dcl, 1);
-	
-	jive::view(graph.root(), stdout);
-	
-	assert(cont1 == i0);
-	assert(cont2 != i0);
-	
-	assert(memb4 != i1);
-	assert(memb3 == i1);
-	
-	auto zero = create_bitconstant(graph.root(), "00000000000000000000000000000000");
-	auto one = create_bitconstant(graph.root(), "10000000000000000000000000000000");
-	auto minus_one = create_bitconstant(graph.root(), "11111111111111111111111111111111");
-	
-	auto a0 = arraysubscript_op::create(i0, bit32, zero);
-	//assert(a0 == top->outputs[0]);
-	auto a1 = arraysubscript_op::create(i0, bit32, one);
-	assert(a1 != i0);
-	arraysubscript_op::create(a1, bit32, minus_one);
-	jive::view(graph.root(), stdout);
-	//assert(tmp == a0);
-	
-	arrayindex_op::create(a1, a0, bit32, bit32);
-	//assert(diff == one);
-	
-	auto diff2 = arrayindex_op::create(i0, i1, bit32, bit32);
+	auto ex0 = graph.add_export(as1, "");
 
-	auto memberof = memberof_op::create(cont3, dcl, 1);
-	auto arraysub = arraysubscript_op::create(i0, bit32, one);
+	view(graph, stdout);
 
-	graph.add_export(memberof, "");
-	graph.add_export(arraysub, "");
-	graph.add_export(diff2, "");
+	assert(is_arraysubscript_node(ex0->origin()->node()));
 
 	memlayout_mapper_simple mapper(4);
-	transform_address(cont3->node(), mapper);
-	transform_address(memberof->node(), mapper);
-	transform_address(diff2->node(), mapper);
-	transform_address(arraysub->node(), mapper);
+	transform_address(ex0->origin()->node(), mapper);
 
 	graph.prune();
-	jive::view(graph.root(), stdout);
+	view(graph, stdout);
+
+	assert(is_bit2addr_node(ex0->origin()->node()));
 
 	return 0;
 }
 
-JIVE_UNIT_TEST_REGISTER("arch/test-address", test_main)
+JIVE_UNIT_TEST_REGISTER("arch/test-arraysubscript", test_arraysubscript)
+
+static int
+test_arrayindex()
+{
+	using namespace jive;
+
+	jive::graph graph;
+
+	auto i0 = graph.add_import(addrtype::instance(), "");
+	auto i1 = graph.add_import(addrtype::instance(), "");
+
+	auto ai1 = arrayindex_op::create(i0, i1, bit32, bit32);
+
+	auto ex0 = graph.add_export(ai1, "");
+
+	view(graph, stdout);
+
+	assert(is_arrayindex_node(ex0->origin()->node()));
+
+	memlayout_mapper_simple mapper(4);
+	transform_address(ex0->origin()->node(), mapper);
+
+	graph.prune();
+	view(graph, stdout);
+
+	assert(is_bitsdiv_node(ex0->origin()->node()));
+
+	return 0;
+}
+
+JIVE_UNIT_TEST_REGISTER("arch/test-arrayindex", test_arrayindex)
